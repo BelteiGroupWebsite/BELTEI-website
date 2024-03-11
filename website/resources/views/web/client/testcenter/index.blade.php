cb94c834ab9a27de5ec79af7a5d208278b03ab6f
Soren Ly <110019765+sorenly333@users.noreply.github.com>
2024-03-11 17:51:51 +0700

    testcenter folder
    i create new testcenter folder

---
+++ website/resources/views/web/client/testcenter/index.blade.php
@@ -0,0 +1,129 @@
+@extends('web.client.school.layout.app')
+
+@section('content')
+
+
+
+<img data-aos="zoom-out-up" class=""
+    src="https://www.beltei.edu.kh/khm/images/homepics/beltei the best quality of education in cambodia.gif"
+    border="0">
+
+    <div>
+        <img src="https://www.beltei.edu.kh/khm/images/specialevents/school_facebook.png" alt="">
+        <img src="https://www.beltei.edu.kh/khm/images/specialevents/beltei_youtube.png" alt="">
+    </div>
+
+    <img data-aos="zoom-out-up" class=""
+        src="https://www.beltei.edu.kh/khm/images/announcement/New_term_2024.jpg" border="0">
+    <img data-aos="zoom-out-up" class=""
+        src="https://www.beltei.edu.kh/khm/images/announcement/03_02_2024_p1.jpg" border="0">
+    <img data-aos="zoom-out-up" class=""
+        src="https://www.beltei.edu.kh/khm/images/announcement/03_02_2024_p2.jpg" border="0">
+    <img data-aos="zoom-out-up" class=""
+        src="https://www.beltei.edu.kh/khm/images/announcement/03_02_2024_p3.jpg" border="0">
+    <img data-aos="zoom-out-up" class=""
+        src="https://www.beltei.edu.kh/khm/images/announcement/03_02_2024_p4.jpg" border="0">
+
+    <div class="dailyNews mt-4" >
+        <h6 class="Muol-Light p-3 bg-success text-white">ព្រឹត្តិការណ៍សំខាន់ៗប្រចាំថ្ងៃ</h6>
+        <hr>
+
+        {{-- Hot News  --}}
+        <a href="" class="card card-body d-flex">
+            <div class="overflow-hidden">
+                <img src="https://www.khmertimeskh.com/wp-content/uploads/2018/03/5-6.jpg" alt="">
+            </div>
+            <div class="p-3">
+                <h6 class="Muol-Light line-height-15">
+                    សា​លា​ ប៊ែល​ធី​ អន្តរ​ជាតិ​ បាន​ប្រារព្ធ​ពិធី​ប្រគល់
+                    ​សញ្ញា​បត្រ​មធ្យម​សិក្សា​ទុតិយ​ភូមិ​ប៊ែល​ធី​ជូន​ដល់​សិស្ស​
+                    ​ជ័យ​លា​ភី​ថ្នាក់​ទី​១២​ ជំ​នាន់​ទី​១៨​ ចំ​នួន​ ១ ០៧៣​នាក់​ (​ក្រុម​ទី​១​)
+                </h6>
+                <p class="Battambang-Regular f14 line-height-15">​
+                    ដែល​បាន​ប្រឡង​ជាប់​សញ្ញា​បត្រ​មធ្យម​សិក្សា​ទុតិ​យ​ភូមិ​ (​បាក់​ឌុប​)​
+                    មក​ពី​សា​ខា​ទី​១​ ដល់​សា​ខា​ទី​១០​ ក្រោម​អធិប​តី​ភាព​ដ៏​ខ្ពង់​ខ្ពស់​របស់​
+                    ឯក​ឧត្តម​បណ្ឌិត​ លី​ ឆេង​ តំណាង​រាស្រ្ត​មណ្ឌល​រាជ​ធានី​ភ្នំពេញ​ ស្ថាប​និក​
+                    និង​ជា​អគ្គ​នា​យក​ ប៊ែល​ធី​ គ្រុប​ ព្រម​ទាំង​លោក​ជំ​ទាវ​។​
+                    ពិ​ធី​នេះ​បាន​ប្រព្រឹត្ត​ទៅ​ នា​ព្រឹក​ថ្ងៃ​សៅរ៍​ ១​រោច​ ខែ​បុស្ស​ ឆ្នាំ​ថោះ​
+                    បញ្ចស័ក​ ព.ស.​២៥៦៧​ ត្រូវ​នឹង​ថ្ងៃ​ទី​២៧​ ខែ​មករា​ ឆ្នាំ​២០២៤​ នៅ​សាល​សន្និ​សីទ​
+                    នៃ​សាកល​វិទ្យា​ល័យ​ ប៊ែលធី​ អន្តរ​ជាតិ​ទី​តាំង​ទី​២​ (​ស្ពាន​អាកាស​ចោម​ចៅ​)​។
+                </p>
+            </div>
+        </a>
+
+        {{-- Other News  --}}
+        <div class="row p-0 m-0">
+
+
+            <a href="" class="col-6 d-flex align-items-center border">
+                <div class="max-width-150 overflow-hidden">
+                    <img class="w-100"
+                        src="https://www.khmertimeskh.com/wp-content/uploads/2018/03/5-6.jpg"
+                        alt="">
+                </div>
+                <div class="p-3 ">
+                    <h6 class="Muol-Light f12 line-height-15">
+                        ប្រឡង​ជាប់​សញ្ញា​បត្រ​មធ្យម​សិក្សា​ទុតិ​យ​ភូមិ</h6>
+                    <p class="Battambang-Regular f12 line-height-15">​
+                        ដែល​បាន​ប្រឡង​ជាប់​សញ្ញា​បត្រ​មធ្យម​សិក្សា​ទុតិ​យ​ភូមិ​ (​បាក់​ឌុប​)​
+                        មក​ពី​សា​ខា​ទី​១​ ដល់​សា​ខា​ទី​១០​
+                    </p>
+                </div>
+            </a>
+
+            <a href="" class="col-6 d-flex align-items-center border">
+                <div class="max-width-150 overflow-hidden">
+                    <img class="w-100"
+                        src="https://www.khmertimeskh.com/wp-content/uploads/2018/03/5-6.jpg"
+                        alt="">
+                </div>
+                <div class="p-3 ">
+                    <h6 class="Muol-Light f12 line-height-15">
+                        ប្រឡង​ជាប់​សញ្ញា​បត្រ​មធ្យម​សិក្សា​ទុតិ​យ​ភូមិ</h6>
+                    <p class="Battambang-Regular f12 line-height-15">​
+                        ដែល​បាន​ប្រឡង​ជាប់​សញ្ញា​បត្រ​មធ្យម​សិក្សា​ទុតិ​យ​ភូមិ​ (​បាក់​ឌុប​)​
+                        មក​ពី​សា​ខា​ទី​១​ ដល់​សា​ខា​ទី​១០​
+                    </p>
+                </div>
+            </a>
+
+            <a href="" class="col-6 d-flex align-items-center border">
+                <div class="max-width-150 overflow-hidden">
+                    <img class="w-100"
+                        src="https://www.khmertimeskh.com/wp-content/uploads/2018/03/5-6.jpg"
+                        alt="">
+                </div>
+                <div class="p-3 ">
+                    <h6 class="Muol-Light f12 line-height-15">
+                        ប្រឡង​ជាប់​សញ្ញា​បត្រ​មធ្យម​សិក្សា​ទុតិ​យ​ភូមិ</h6>
+                    <p class="Battambang-Regular f12 line-height-15">​
+                        ដែល​បាន​ប្រឡង​ជាប់​សញ្ញា​បត្រ​មធ្យម​សិក្សា​ទុតិ​យ​ភូមិ​ (​បាក់​ឌុប​)​
+                        មក​ពី​សា​ខា​ទី​១​ ដល់​សា​ខា​ទី​១០​
+                    </p>
+                </div>
+            </a>
+
+            <a href="" class="col-6 d-flex align-items-center border">
+                <div class="max-width-150 overflow-hidden">
+                    <img class="w-100"
+                        src="https://www.khmertimeskh.com/wp-content/uploads/2018/03/5-6.jpg"
+                        alt="">
+                </div>
+                <div class="p-3 ">
+                    <h6 class="Muol-Light f12 line-height-15">
+                        ប្រឡង​ជាប់​សញ្ញា​បត្រ​មធ្យម​សិក្សា​ទុតិ​យ​ភូមិ</h6>
+                    <p class="Battambang-Regular f12 line-height-15">​
+                        ដែល​បាន​ប្រឡង​ជាប់​សញ្ញា​បត្រ​មធ្យម​សិក្សា​ទុតិ​យ​ភូមិ​ (​បាក់​ឌុប​)​
+                        មក​ពី​សា​ខា​ទី​១​ ដល់​សា​ខា​ទី​១០​
+                    </p>
+                </div>
+            </a>
+
+
+        </div>
+
+        
+    </div>
+
+
+@endsection
---
+++ website/resources/views/web/client/testcenter/layout/left_side_bar.blade.php
@@ -0,0 +1,41 @@
+<div class="sidebar min-width-250 max-width-300 shadow-sm p-0 m-0 content-translate-font px-1">
+    <ul class="text-capitalize list-group f14">
+        <a href="{{ route('school.') }}">
+            <h6 class="text-uppercase text-center py-2 bg-header-color gold ">{{ __('school/app.A') }}</h6>
+        </a>
+        <a href="{{ route('school.about.welcome') }}" class="list-group-item text-white">{{ __('school/app.A1') }}</a>
+        <a href="{{ route('school.about.purpose') }}" class="list-group-item text-white">{{ __('school/app.A2') }}</a>
+        <a href="{{ route('school.about.vision') }}" class="list-group-item text-white">{{ __('school/app.A3') }}</a>
+        <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('school/app.A4') }}</a>
+        <a href="{{ asset('asset/img/school/organization_chart.jpg') }}" target="blank" class="list-group-item text-white">{{ __('school/app.A5') }}</a>
+        <a href="{{ route('school.about.whybeltei') }}" class="list-group-item text-white">{{ __('school/app.A6') }}</a>
+    </ul>
+    <ul class="text-capitalize list-group f14">
+        <a href="{{ route('school.') }}">
+            <h6 class="text-uppercase text-center py-2 bg-header-color gold ">{{ __('school/app.B') }}</h6>
+        </a>
+        <a href="{{ route('school.about.welcome') }}" class="list-group-item text-white">{{ __('school/app.B1') }}</a>
+        <a href="{{ route('school.about.purpose') }}" class="list-group-item text-white">{{ __('school/app.B2') }}</a>
+        <a href="{{ route('school.about.vision') }}" class="list-group-item text-white">{{ __('school/app.B3') }}</a>
+        <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('school/app.B4') }}</a>
+        <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('school/app.B5') }}</a>
+    </ul>
+    <ul class="text-capitalize list-group f14">
+        <a href="{{ route('school.') }}">
+            <h6 class="text-uppercase text-center py-2 bg-header-color gold ">{{ __('school/app.C') }}</h6>
+        </a>
+        <a href="{{ route('school.about.welcome') }}" class="list-group-item text-white">{{ __('school/app.C1') }}</a>
+        <a href="{{ route('school.about.purpose') }}" class="list-group-item text-white">{{ __('school/app.C2') }}</a>
+        <a href="{{ route('school.about.vision') }}" class="list-group-item text-white">{{ __('school/app.C3') }}</a>
+        <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('school/app.C4') }}</a>
+        <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('school/app.C5') }}</a>
+        <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('school/app.C6') }}</a>
+        <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('school/app.C7') }}</a>
+        <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('school/app.C8') }}</a>
+        <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('school/app.C9') }}</a>
+        <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('school/app.C10') }}</a>
+        <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('school/app.C11') }}</a>
+        <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('school/app.C12') }}</a>
+    </ul>
+
+</div>
