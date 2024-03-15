33aa542fcdc9b00efcef18a733a7a2a9519429f9
[soksereyvisalBIU] <121630407+soksereyvisalBIU@users.noreply.github.com>
2024-03-15 18:50:53 +0700

    Update welcome.blade.php

--- website/resources/views/welcome.blade.php
+++ website/resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
@@ -37,4 +37,46 @@
     </script>
 </body>
 
+</html> --}}
+
+
+<html>
+<body>
+<head>
+<title>BELTEI Group Structure</title>
+<table align="center" border="0">
+<tbody>
+<tr style="text-align: center;">
+<!--<td><img src="images/new structure beltei group 2017.jpg" usemap="#f_inbound_tour" border="0" /></td>-->
+<td><img src="{{ asset('asset/img/new-structure-beltei-group-official-2019.jpg') }}" usemap="#f_inbound_tour" border="0" /></td>
+<!--<td><img src="images/bt-home/frame_inbound_tour.jpg" alt="Inbound_Tour" usemap="#f_inbound_tour" border="0" />-->
+<map name="f_inbound_tour">
+   <area shape="circle" coords="646,68,67" href="beltei_group_logos.html" target="_blank"></a>
+   <area shape="rect" coords="7,279,186,499" href="https://www.belteigroup.com.kh/bconk/"></a>
+   <area shape="rect" coords="199,281,368,495" href="{{ url('school') }}"></a>
+   <area shape="rect" coords="379,282,548,493" href="{{ url('beltei_university') }}"></a>
+   <area shape="rect" coords="560,281,729,495" href="https://www.beltei.edu.kh/bire/"></a>
+   <!--<area shape="rect" coords="737,281,909,494" href="https://www.beltei.edu.kh/internationaltests/"></a>-->
+   <area shape="rect" coords="737,281,909,494" href="https://www.beltei.edu.kh/internationaltests"></a>
+   <area shape="rect" coords="920,281,1091,496" href="https://www.belteitour.com.kh/btk/index.php"></a>
+   <area shape="rect" coords="1114,281,1281,494" href="https://www.belteigroup.com.kh/charitykhm/"></a>
+
+</map> 
+</td>
+</tr>
+<tr style="text-align: center;">
+<td><img src="images/beltei_motto_khmer.gif" border="0" /></td>
+</tr>
+<tr style="text-align: center;">
+<td><img src="images/beltei_motto_eng.gif" border="0" /></td>
+</tr>
+<tr style="text-align: center;color:blue;font-family: Helvetica Condensed;font-size:200%;font-weight: bold";>
+<td>www.beltei.edu.kh</td>
+</tr>
+<tr style="text-align: center;color:blue;font-family: Helvetica Condensed;font-size:200%;font-weight: bold";>
+<td><a href="https://belteigroup.com.kh/beltei_group_logos.html" target="_blank">click here to download all BELTEI's Logos<img src="images/beltei-group-logo-download.png" border="0" width="50" height=" 50"/></td></a>
+</tr>
+</tbody>
+</table>
+</body>
 </html>
