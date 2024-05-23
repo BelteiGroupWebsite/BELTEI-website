09a4f4db387b16f7cad78f8c7ac76e05d0bce8f6
Chhaty <chhatyhoeurn@gmail.com>
2024-05-23 19:28:37 +0700

    hi

---
+++ website/resources/views/web/client/beltei_university/speaking/english_speaking_year4.blade.php
@@ -0,0 +1,18 @@
+@extends('web.client.beltei_university.layout.app')
+
+@section('content')
+    <h2 class="title-translate-font my-3 underline">{{ __('relation/about/welcome.header') }}</h2>
+
+
+
+
+    <div class="text-start">
+        <div class="content-translate-font p-lg-4 ul-none-style">
+            <img class="float-start rounded shadow-sm p-3"
+                src="https://www.beltei.edu.kh/khm/images/beltei_pic/H.E_Dr_Ly_chheng_beltei.jpg" alt="">
+            <div class="text-justify">
+                {!! __('relation/about/welcome.welcome') !!}
+            </div>
+        </div>
+    </div>
+@endsection
