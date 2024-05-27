e9f55186c7dcc9a2324b2d921a68c064839838d8
Chhaty <chhatyhoeurn@gmail.com>
2024-05-27 08:22:49 +0700

    hi

--- website/resources/views/web/client/beltei_university/speaking/khmer_speaking_year4.blade.php
+++ website/resources/views/web/client/beltei_university/speaking/khmer_speaking_year4.blade.php
@@ -1,18 +1,54 @@
 @extends('web.client.beltei_university.layout.app')
 
+
 @section('content')
-    <h2 class="title-translate-font my-3 underline">{{ __('relation/about/welcome.header') }}</h2>
 
+    <div class="px-2">
 
+        <h3 class="title-translate-font p-3 bg-success text-white">{{ __('school/public-speaking/public-speaking.header')  }}</h3>
+        <hr>
+        <div>
+            <iframe class="w-100 aspect-ratio-16"
+                src="https://www.youtube.com/embed/cBoCmimVUm0?si=mhpzod3BjZm6LePQ"></iframe>
+        </div>
 
-
-    <div class="text-start">
-        <div class="content-translate-font p-lg-4 ul-none-style">
-            <img class="float-start rounded shadow-sm p-3"
-                src="https://www.beltei.edu.kh/khm/images/beltei_pic/H.E_Dr_Ly_chheng_beltei.jpg" alt="">
-            <div class="text-justify">
-                {!! __('relation/about/welcome.welcome') !!}
+        <hr>
+        <h5 class="mt-4 mb-2 content-translate-font mt-3 mb-1">{{ __('school/public-speaking/public-speaking.thewinner')  }} {{ __('school/public-speaking/public-speaking.n1') }}{{ __('school/public-speaking/public-speaking.n2') }} {{ __('school/public-speaking/public-speaking.in') }} 2023 ({{ __('school/public-speaking/public-speaking.Batch') }} 8)</h5>
+        <div class="row">
+            <div class="col-4 height-200 content-translate-font">
+                <iframe class="w-100 rounded shadow-sm " width="" height=""
+                    src="https://www.youtube.com/embed/cBoCmimVUm0?si=mhpzod3BjZm6LePQ" title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <p class="goldenrod font-bold">
+                        {{ __('school\public-speaking\public-speaking.place') }} {{ __('school/public-speaking/public-speaking.n1') }}
+                    </p>
+            </div>
+            <div class="col-4 height-200 content-translate-font">
+                <iframe class="w-100 rounded shadow-sm " width="" height=""
+                    src="https://www.youtube.com/embed/rhurJc8PfzE?si=8RctBx_WbbHPAZZ5" title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <p class="goldenrod font-bold">
+                        {{ __('school\public-speaking\public-speaking.place') }} {{ __('school/public-speaking/public-speaking.n2') }}
+                    </p>
+            </div>
+            <div class="col-4 height-200 content-translate-font">
+                <iframe class="w-100 rounded shadow-sm " width="" height=""
+                    src="https://www.youtube.com/embed/_ujJv00Q3Rg?si=LBg7jHAA7JksFFIB" title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <p class="goldenrod font-bold">
+                        {{ __('school\public-speaking\public-speaking.place') }} {{ __('school/public-speaking/public-speaking.n3') }}
+                    </p>
             </div>
         </div>
+
+        <hr class="border border-success border-3">
+
     </div>
+
 @endsection
