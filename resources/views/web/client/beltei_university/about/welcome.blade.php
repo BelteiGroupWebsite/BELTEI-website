1443939d84a125e18ef37bfc7ea9a4d69ee06500
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-07-05 16:30:55 +0700

    Update welcome.blade.php

--- resources/views/web/client/beltei_university/about/welcome.blade.php
+++ resources/views/web/client/beltei_university/about/welcome.blade.php
@@ -9,7 +9,7 @@
     <div class="text-start">
         <div class="content-translate-font p-lg-4 ul-none-style">
             <img class="float-start rounded shadow-sm p-3"
-                src="https://www.beltei.edu.kh/khm/images/beltei_pic/H.E_Dr_Ly_chheng_beltei.jpg" alt="">
+                src="https://media.licdn.com/dms/image/C5603AQEfV5tW79ZMJg/profile-displayphoto-shrink_200_200/0/1593411121688?e=2147483647&v=beta&t=e5O6w9XdIM4BkXWltAqsN7_9ku4WEvY6iHWBuOxH0gM" alt="">
             <div class="text-justify">
                 {!! __('beltei_university/about/welcome.welcome') !!}
             </div>
