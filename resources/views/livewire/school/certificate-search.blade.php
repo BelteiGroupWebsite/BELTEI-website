3241bfdb0b44652bbe0cfd0b076b179f982972e5
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-07-12 09:51:56 +0700

    Update certificate-search.blade.php

--- resources/views/livewire/school/certificate-search.blade.php
+++ resources/views/livewire/school/certificate-search.blade.php
@@ -53,7 +53,7 @@
                     </td>
                     <td>
                         <a href="{{ asset('storage/upload/certificate/school/'.$programId.'/'.$gradeId.'/'.$batchId.'/moey/'.$student->moey_no.'.jpg') }}">
-                            <img style="max-width: 50px" loading="lazy" src="{{ asset('storage/upload/certificate/school/'.$programId.'/'.$gradeId.'/'.$batchId.'/beltei/'.$student->moey_no.'.jpg') }}" alt="Moey">
+                            <img style="max-width: 50px" loading="lazy" src="{{ asset('storage/upload/certificate/school/'.$programId.'/'.$gradeId.'/'.$batchId.'/moey/'.$student->moey_no.'.jpg') }}" alt="Moey">
                         </a>
                     </td>
                     {{-- <td>
