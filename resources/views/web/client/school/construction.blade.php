e447962c7c2ba2479a1f4a7ef932686a201c270b
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-07-19 18:06:22 +0700

    Update construction.blade.php

--- resources/views/web/client/school/construction.blade.php
+++ resources/views/web/client/school/construction.blade.php
@@ -7,6 +7,6 @@
         <img class="w-80" src="{{ asset("asset/img/underconstruction.jpg") }}" alt="">
     </div>
 
-    @include('web.client.beltei_tours_travel.layout.share.news')
+    @include('web.client.school.layout.share.news')
     
 @endsection
