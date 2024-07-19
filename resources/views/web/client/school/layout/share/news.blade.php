7e0b97a0fc700db066f922099848565ce47ccefb
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-07-19 15:47:02 +0700

    Update news.blade.php

--- resources/views/web/client/school/layout/share/news.blade.php
+++ resources/views/web/client/school/layout/share/news.blade.php
@@ -34,7 +34,7 @@
                         @if ($item->language_id == 1)
                             <div onclick="toggleDescription(this)" class="cursor-pointer "
                                 data-fulltext="{{ $item->description }}">
-                                {{ Str::limit($item->description, 500) }}
+                                {{ __(Str::limit($item->description, 500)) }}
                             </div>
                         @endif
                     @endforeach
