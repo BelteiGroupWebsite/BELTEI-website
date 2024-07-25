7c07235743d3ac87e83337730858796aa78de98d
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-07-25 10:36:33 +0700

    update

--- resources/views/web/client/contruction/layout/right_side_bar.blade.php
+++ resources/views/web/client/contruction/layout/right_side_bar.blade.php
@@ -69,22 +69,6 @@
         </a>
     </ul>
 
-    <ul class="text-capitalize list-group f14 unhover-padding">
-        <a href="{{ route('school.') }}">
-            <h6 class="text-uppercase text-center py-2 bg-header-color gold ">Visitors : {{ $visitor->count() }}</h6>
-            <table class="table content-translate-font text-white">
-                @foreach ($countries as $country)
-                    <tr>
-                        <td>
-                            {{ $country->region ." / ". $country->name }}
-                        </td>
-                        <td>
-                            {{ $country->visitors->count() }} visitors
-                        </td>
-                    </tr>
-                @endforeach
-            </table>
-        </a>
-    </ul>
+    @include('web.client.share.visitor')
     
 </div>
