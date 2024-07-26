6ff6483d4eadcf145d3e0acd08c3cba71f9d7073
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-07-26 08:32:47 +0700

    Update right_side_bar.blade.php

--- resources/views/web/client/school/layout/right_side_bar.blade.php
+++ resources/views/web/client/school/layout/right_side_bar.blade.php
@@ -64,25 +64,25 @@
         <a class="list-group-item text-center gold font-bold">{{ __('school/app.generalProgram') }}</a>
         {{-- <a class="list-group-item" href="{{ route('school.certificate' , ['program'=>1 , 'grade' => 12]) }}" class="list-group-item text-danger font-bold text-center"> --}}
         <a class="list-group-item" href="{{ route('school.certificate' , 1) }}" class="list-group-item text-danger font-bold text-center">
-            <div class="d-flex align-items-center justify-content-center Red-color-border rounded " style="border:outset 3px ;">
+            <div class="d-flex align-items-center justify-content-between Red-color-border rounded " style="border:outset 3px ;">
                 <img class="max-width-50 shadow-sm border-end Red-color-border" src="{{ asset('asset/img/school/app/1.jpg') }}" alt="">
-                <p class="text-white text-center Khmer-OS-Koulen px-2">
+                <p class="text-white text-center Khmer-OS-Koulen px-2 w-100">
                     {{ __('school/app.highSchoolDiploma') }}
                 </p>
             </div>
         </a>
         <a class="list-group-item" href="{{ route('school.certificate' , 2) }}" class="list-group-item text-white font-bold text-center">
-            <div class="d-flex align-items-center justify-content-center Red-color-border rounded " style="border:outset 3px ;">
+            <div class="d-flex align-items-center justify-content-between Red-color-border rounded " style="border:outset 3px ;">
                 <img class="max-width-50 shadow-sm border-end Red-color-border" src="{{ asset('asset/img/school/app/2.jpg') }}" alt="">
-                <p class="text-white text-center Khmer-OS-Koulen px-2">
+                <p class="text-white text-center Khmer-OS-Koulen px-2 w-100">
                     {{ __('school/app.juniorHighSchool') }}
                 </p>
             </div>
         </a>
         <a class="list-group-item" href="{{ route('school.certificate' , 3) }}" class="list-group-item text-white font-bold text-center">
-            <div class="d-flex align-items-center justify-content-center Red-color-border rounded " style="border:outset 3px ;">
+            <div class="d-flex align-items-center justify-content-between Red-color-border rounded " style="border:outset 3px ;">
                 <img class="max-width-50 shadow-sm border-end Red-color-border" src="{{ asset('asset/img/school/app/3.jpg') }}" alt="">
-                <p class="text-white text-center Khmer-OS-Koulen px-2">
+                <p class="text-white text-center Khmer-OS-Koulen px-2 w-100">
                     {{ __('school/app.primaryCertificate') }}
                 </p>
             </div>
@@ -90,17 +90,17 @@
         
         <a class="list-group-item text-center gold font-bold">{{ __('school/app.ESLProgram') }}</a>
         <a class="list-group-item" href="{{ route('school.certificate' , 4) }}" class="list-group-item text-white font-bold text-center">
-            <div class="d-flex align-items-center justify-content-center Red-color-border rounded " style="border:outset 3px ;">
+            <div class="d-flex align-items-center justify-content-between Red-color-border rounded " style="border:outset 3px ;">
                 <img class="max-width-50 shadow-sm border-end Red-color-border" src="{{ asset('asset/img/school/app/4.jpg') }}" alt="">
-                <p class="text-white text-center Khmer-OS-Koulen px-2">
+                <p class="text-white text-center Khmer-OS-Koulen px-2 w-100">
                     {{ __('school/app.ESLDiploma') }}
                 </p>
             </div>
         </a>
         <a class="list-group-item" href="{{ route('school.certificate' , 5) }}" class="list-group-item text-white font-bold text-center">
-            <div class="d-flex align-items-center justify-content-center Red-color-border rounded " style="border:outset 3px ;">
+            <div class="d-flex align-items-center justify-content-between Red-color-border rounded " style="border:outset 3px ;">
                 <img class="max-width-50 shadow-sm border-end Red-color-border" src="{{ asset('asset/img/school/app/5.jpg') }}" alt="">
-                <p class="text-white text-center Khmer-OS-Koulen px-2">
+                <p class="text-white text-center Khmer-OS-Koulen px-2 w-100">
                     {{ __('school/app.ESLlevel6') }}
                 </p>
             </div>
@@ -116,17 +116,17 @@
 
         <a class="list-group-item text-center gold font-bold">{{ __('school/app.IMA') }}</a>
         <a class="list-group-item" href="{{ route('school.certificate' , 7) }}" class="list-group-item text-white font-bold text-center">
-            <div class="d-flex align-items-center justify-content-center Red-color-border rounded " style="border:outset 3px ;">
+            <div class="d-flex align-items-center justify-content-between Red-color-border rounded " style="border:outset 3px ;">
                 <img class="max-width-50 shadow-sm border-end Red-color-border" src="{{ asset('asset/img/school/app/7.jpg') }}" alt="">
-                <p class="text-white text-center Khmer-OS-Koulen px-2">
+                <p class="text-white text-center Khmer-OS-Koulen px-2 w-100">
                     {{ __('school/app.IMA11') }}
                 </p>
             </div>
         </a>
         <a class="list-group-item" href="{{ route('school.certificate' , 8) }}" class="list-group-item text-white font-bold text-center">
-            <div class="d-flex align-items-center justify-content-center Red-color-border rounded " style="border:outset 3px ;">
+            <div class="d-flex align-items-center justify-content-between Red-color-border rounded " style="border:outset 3px ;">
                 <img class="max-width-50 shadow-sm border-end Red-color-border" src="{{ asset('asset/img/school/app/8.jpg') }}" alt="">
-                <p class="text-white text-center Khmer-OS-Koulen px-2">
+                <p class="text-white text-center Khmer-OS-Koulen px-2 w-100">
                     {{ __('school/app.IMA6') }}
                 </p>
             </div>
