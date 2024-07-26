f37fd44251fa43c49965fcaf35a346666e91eae4
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-07-26 08:37:57 +0700

    Update index.blade.php

--- resources/views/web/client/school/outstanding-student/grade-A/index.blade.php
+++ resources/views/web/client/school/outstanding-student/grade-A/index.blade.php
@@ -9,7 +9,7 @@
 
 
                 <div>
-                    <h5 class="title-translate-font bg-success p-2 text-white text-center py-3">
+                    <h5 class="title-translate-font bg-success p-2 text-white text-center">
 
                         {{ $gradeA->start_academic_year }}  = {{ $gradeA->studentGradeA->count() }}  
                     
