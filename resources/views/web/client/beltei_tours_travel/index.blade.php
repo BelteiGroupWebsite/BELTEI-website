2f43a4798b6a984e0698a54b3d4be18cdf7b308a
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-07-26 11:19:04 +0700

    Update index.blade.php

--- resources/views/web/client/beltei_tours_travel/index.blade.php
+++ resources/views/web/client/beltei_tours_travel/index.blade.php
@@ -14,14 +14,14 @@
         <img class="w-80 my-2" src="{{ asset('asset/img/BTG.png') }}" alt="">
     </div>
 
-    <iframe width="500" height="280" src="https://www.youtube.com/embed/6I8WENHBtOc?si=izlAt6OZxnbO-vPT" title="YouTube video player" frameborder="0" allow="autoplay; " allowfullscreen></iframe>
+    <iframe class="w-80 my-2" width="500" height="280" src="https://www.youtube.com/embed/6I8WENHBtOc?si=izlAt6OZxnbO-vPT" title="YouTube video player" frameborder="0" allow="autoplay; " allowfullscreen></iframe>
 
 
-    <img data-aos="zoom-out-up" class="" src="https://www.belteitour.com.kh/btk/images/announcement/beltei_tour_new_promotion.jpg"
+    <img data-aos="zoom-out-up" class="w-80 my-2" src="https://www.belteitour.com.kh/btk/images/announcement/beltei_tour_new_promotion.jpg"
         border="0">
-    <img data-aos="zoom-out-up" class="" src="https://www.belteitour.com.kh/btk/images/announcement/beltei_tour_in_cambodia_new_promotion_to_china.jpg"
+    <img data-aos="zoom-out-up" class="w-80 my-2" src="https://www.belteitour.com.kh/btk/images/announcement/beltei_tour_in_cambodia_new_promotion_to_china.jpg"
         border="0">
-    <img data-aos="zoom-out-up" class="" src="https://www.belteitour.com.kh/btk/images/homepics/office_1.jpg"
+    <img data-aos="zoom-out-up" class="w-80 my-2" src="https://www.belteitour.com.kh/btk/images/homepics/office_1.jpg"
         border="0">
 
 
