99c280d1cd50cb94fa760a9cd87caab134f855d0
Chhaty <chhatyhoeurn@gmail.com>
2024-08-01 08:28:42 +0700

    Update debate12.blade.php

--- resources/views/web/client/school/debate/english/debate12.blade.php
+++ resources/views/web/client/school/debate/english/debate12.blade.php
@@ -7,7 +7,7 @@
         <hr>
         <div>
             <iframe class="w-100 aspect-ratio-16"  id="vd-container"
-                src="https://www.youtube.com/embed/RaybuOZlW7k?si=FM-y2LLsRpyxdQ-0" title="YouTube video player"
+                src="https://www.youtube.com/embed/3KZNpkgpSp8?si=UCiBYKsEmrREJ1fI" title="YouTube video player"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 allowfullscreen></iframe>
