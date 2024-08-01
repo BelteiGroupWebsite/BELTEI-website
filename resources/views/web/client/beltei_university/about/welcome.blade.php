6bdaa4ec9535d71b96783f23548f9b07470dd28d
Chhaty <chhatyhoeurn@gmail.com>
2024-08-01 19:33:35 +0700

    update

--- resources/views/web/client/beltei_university/about/welcome.blade.php
+++ resources/views/web/client/beltei_university/about/welcome.blade.php
@@ -13,8 +13,10 @@
             <div class="text-justify">
                 {!! __('beltei_university/about/welcome.welcome') !!}
             </div>
-            <img class="float-start rounded"
+            <img class="float-start rounded" style="width: 200px"
                 src="{{ asset('asset/img/biu/app/HEDr_sign.png')}}" alt="">
+                <p class="title-translate-font"> បណ្ឌិត លី ឆេង</p>
+
         </div>
     </div>
 @endsection
