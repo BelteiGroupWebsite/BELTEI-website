5fe05ba6033270db0bd0a58aaa6f72c7b8f28289
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-08-02 18:58:16 +0700

    library

--- resources/views/web/client/beltei_university/facilities/library.blade.php
+++ resources/views/web/client/beltei_university/facilities/library.blade.php
@@ -10,6 +10,32 @@
     <div class="text-start content-translate-font my-3 px-2">
         {!! __('beltei_university/facilities/library.description') !!}
     </div>
+    <div class="row">
+        <div class="col-6">
+            <img class="w-100" src="{{ asset('asset/img/university/facility/library/1.png') }}" alt="">
+        </div>
+        <div class="col-6">
+            <img class="w-100" src="{{ asset('asset/img/university/facility/library/2.png') }}" alt="">
+        </div>
+        <div class="col-6">
+            <img class="w-100" src="{{ asset('asset/img/university/facility/library/3.png') }}" alt="">
+        </div>
+        <div class="col-6">
+            <img class="w-100" src="{{ asset('asset/img/university/facility/library/4.png') }}" alt="">
+        </div>
+        <div class="col-6">
+            <img class="w-100" src="{{ asset('asset/img/university/facility/library/5.png') }}" alt="">
+        </div>
+        <div class="col-6">
+            <img class="w-100" src="{{ asset('asset/img/university/facility/library/6.png') }}" alt="">
+        </div>
+        <div class="col-6">
+            <img class="w-100" src="{{ asset('asset/img/university/facility/library/7.png') }}" alt="">
+        </div>
+        <div class="col-6">
+            <img class="w-100" src="{{ asset('asset/img/university/facility/library/8.png') }}" alt="">
+        </div>
+    </div>
 </div>
 
 
