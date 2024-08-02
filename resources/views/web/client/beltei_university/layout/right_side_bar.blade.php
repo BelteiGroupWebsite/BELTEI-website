f30bd3debf42982f4a3f65106ccf3485a6f746c9
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-08-02 10:19:10 +0700

    Update right_side_bar.blade.php

--- resources/views/web/client/beltei_university/layout/right_side_bar.blade.php
+++ resources/views/web/client/beltei_university/layout/right_side_bar.blade.php
@@ -94,7 +94,7 @@
                 </li>
                 <li class="list-group-item p-0">
                     <a href="https://libraryhub-manila.blogspot.com/p/home.html" target="_blank">
-                        <img style="max-height: 75px; width: 80% !important; object-fit: fill;" class="justify-content-center" src="{{ asset('asset/img/university/app/rightsidebar/cambodia/6.png') }}" alt="">
+                        <img class="w-100 justify-content-center" src="{{ asset('asset/img/university/app/rightsidebar/cambodia/6.png') }}" alt="">
                     </a>
                 </li>
                 <li class="list-group-item p-0">
