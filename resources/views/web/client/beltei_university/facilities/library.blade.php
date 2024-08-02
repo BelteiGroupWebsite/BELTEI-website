c884ad36b086f96a366e53b06da4da3a4a617f8a
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-08-02 18:38:32 +0700

    Update library.blade.php

--- resources/views/web/client/beltei_university/facilities/library.blade.php
+++ resources/views/web/client/beltei_university/facilities/library.blade.php
@@ -7,7 +7,7 @@
     <div>
         <h3 class="title-translate-font p-3 bg-success text-white">{{ __('beltei_university/facilities/library.header')  }}</h3>
     </div>
-    <div class="text-start content-translate-font my-3">
+    <div class="text-start content-translate-font my-3 px-2">
         {!! __('beltei_university/facilities/library.description') !!}
     </div>
 </div>
