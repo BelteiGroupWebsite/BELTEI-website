09f58c87d4e3475f85b79e78a94a89e0b59579ac
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-08-02 09:21:28 +0700

    intensive cource

--- resources/lang/en/beltei_university/mainprogram/prebachelor.php
+++ resources/lang/en/beltei_university/mainprogram/prebachelor.php
@@ -3,109 +3,12 @@
 
 return [
 
-    'header' => "Inrensive Course (English and Computer)",
+    'header' => "Intensive Course",
 
     'description' => '
 
-    <div>
-        <h5 class="font-bold my-3"><span class="RED-color">Course Overview</span></h5>
-        <ul>
-            <li>
-            BELTEI International University is a university where English is used as medium of instruction of all subjects. In each faculty, local and foreign lecturer conduct course in English using an international program of study.            </li>
-            <li>
-            (BELTEI\'s) Bridging Course is intended to raise the English language skills of students to a sufficient level, so they will have the ability and be ready for higher education effectively by focusing on the 5 macro-skills such as listening, speaking, writing, reading and thinking at 2 different levels.
-            </li>
-            <li>
-            Upon completion the bridging course, students will be able to obtain a certificate as a confirmation of study issued by the university, and students can attend the foundation year course automatically (For the students who hold High School Diploma or the equivalent).            </li>
-        </ul>
-        <h5 class="font-bold my-3"><span class="Green-color">Admission Requirements</span></h5>
-        <ul>
-            <li>
-            Both female and male candidates from 15 years old are allowed to attend the Bridging Course regardless any qualifications.</li>
-            <li>
-            Candidates are required to take an enrollment test before the admission.
-            </li>
-        </ul>
-        <h5 class="font-bold my-3"><span class="Green-color">Intake Dates</span></h5>
-        <ul>
-            <li>
-            First Intake: 1st October</li>
-            <li>
-            Second Intake: 1st October
-            </li>
-        </ul>
-    </div>
-
-    <div>
-        <table class="table table-bordered text-center vertical-middle">
-            <tbody>
-
-                <tr class="BEng-color-bg">
-                    <td class="label" colspan="2">Date and Time</td>
-                </tr>
-
-                <tr class="BCA-color-bg">
-                    <td><strong>Monday to Friday</strong></td>
-                    <td><strong>Satursday to Sunday</strong></td>
-                </tr>
-
-                <tr class="BCA-color-bg">
-                    <td>Morning  : 08:00 - 11:15 <br>Afternoon : 02:00 - 05:15 <br>Evening : 05:30 - 08:30</td>
-                    <td>Satursday: 08:00 - 11:30 <br>01:30 - 05:30</td>
-                </tr>
-
-                <tr  class="BCA-color-bg">
-                    <td></td>
-                    <td>Sunday 08:00 - 11:30 <br>01:30 - 05:30</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-
-<div>
-<table class="table table-bordered text-center vertical-middle">
-    <tbody>
-
-        <tr class="BEng-color-bg">
-            <td class="label" colspan="2">Program of Study (One year Program)</td>
-        </tr>
-
-        <tr class="BCA-color-bg">
-            <td><strong>Semester1 / BC1</strong></td>
-            <td><strong>Semester2 / BC2</strong></td>
-        </tr>
-
-        <tr class="BCA-color-bg">
-            <td class="text-start">
-            Skills Focused:<br>
-            1. Reading and Vocabulary 1<br>
-            2. Basic Writing 1<br>
-            3. Grammar and Composition 1<br>
-            4. Listening and Speaking 1<br>
-            5. Short Stories 1<br>
-            <br>
-            Duration: 6 months<br>
-            <br>
-            Teachers: 2 Khmers and 1 Foreigner</td><br>
-            <td class="text-start">
-            Skills Focused:<br>
-            1. Reading and Vocabulary 2<br>
-            2. Basic Writing 2<br>
-            3. Grammar and Composition 2<br>
-            4. Listening and Speaking 2<br>
-            5. Short Stories 2<br>
-            <br>
-            Duration: 6 months<br>
-            <br>
-            Teachers: 2 Khmers and 1 Foreigner</td>
-        </tr>
-        </tr>
-    </tbody>
-</table>
-</div>
-<div class="text-center">
-<a href="https://www.beltei.edu.kh/dl/Application-Form-Bachelor-Approved.pdf"> <img src="https://www.beltei.edu.kh/biue/images/Title/beltei-international-university-in-cambodia-bridgingcourse-application-form.jpg" alt=""></a>
-</div>
+<p><strong><span> Course   to  the  in  speaking, listening, writing and reading of students who have enrolled in National Program and wish to  to International  in year 2  want to get a job after completing in year 1 successfully. Intensive  is  into two courses,  Course 1 and Intensive Course 2, which take a full year of study equal to 30 credits.   choose any  to study  does  overlap with   1 study and  successful completion, the university will</span></strong></p>
+<div style="color: red; font-size: 12px; width: 600px; margin: 0 auto; text-align: center;">Created with <a href="https://wordtohtml.net/">WordToHTML.net</a> trial.</div>
 
 
     '
--- resources/views/web/client/beltei_university/mainprogram/prebachelor.blade.php
+++ resources/views/web/client/beltei_university/mainprogram/prebachelor.blade.php
@@ -7,11 +7,34 @@
 
     <h2 class="title-translate-font my-3 underline Blue-color">{{ __('beltei_university/mainprogram/prebachelor.header') }}</h2>
 
+    <div class="row w-100">
+        <div class="col-8">
+            <img class="w-100" src="{{ asset('asset/img/university/main_degree/cc/1.png') }}" alt="">
+        </div>
+        <div class="col-4">
+            <img class="w-100" src="{{ asset('asset/img/university/main_degree/cc/2.png') }}" alt="">
+        </div>
+    </div>
 
     <div class="text-start content-translate-font  text-justify ">
         {!! __('beltei_university/mainprogram/prebachelor.description') !!}
     </div>
 
+    <div class="row">
+        <div class="col-4">
+            <img src="{{ asset('asset/img/university/main_degree/cc/3.png') }}" alt="">
+        </div>
+        <div class="col-4">
+            <img src="{{ asset('asset/img/university/main_degree/cc/4.png') }}" alt="">
+        </div>
+        <div class="col-4">
+            <img src="{{ asset('asset/img/university/main_degree/cc/5.png') }}" alt="">
+        </div>
+        <div class="col-12">
+            <img src="{{ asset('asset/img/university/main_degree/cc/6.png') }}" alt="">
+        </div>
+    </div>
+    
 </div>
 
 @endsection
