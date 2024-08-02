03ebf58f8928b50cc344105d8ce9e8253cad6556
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-08-02 09:07:15 +0700

    Update whyBeltei.blade.php

--- resources/views/web/client/beltei_university/about/whyBeltei.blade.php
+++ resources/views/web/client/beltei_university/about/whyBeltei.blade.php
@@ -7,7 +7,7 @@
         <h4 class="title-translate-font my-3 underline Blue-color">{{ __('beltei_university/about/whyBeltei.header') }}</h4>
 
         <div>
-            <img class="w-75 text-center" src="{{ asset('asset/img/university/about/whybeltei.jpg') }}" alt="">
+            <img class="w-75 text-center my-3" src="{{ asset('asset/img/university/about/whybeltei.jpg') }}" alt="">
         </div>
 
         <div class="content-translate-font text-justify">
