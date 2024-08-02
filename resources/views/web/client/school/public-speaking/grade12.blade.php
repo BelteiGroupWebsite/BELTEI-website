1a192c39b4af69f811bde4ad48649516fb977b34
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-08-02 16:27:53 +0700

    Update grade12.blade.php

--- resources/views/web/client/school/public-speaking/grade12.blade.php
+++ resources/views/web/client/school/public-speaking/grade12.blade.php
@@ -12,13 +12,51 @@
         <hr>
         <div>
             <iframe id="vd-container" class="w-100 aspect-ratio-16"
-                src="https://www.youtube.com/embed/PMj3yQAnTEs?si=E3A-vjX2Twc3-cAP&autoplay=1" title="YouTube video player"
+                src="https://www.youtube.com/embed/avZyd_SInnI?si=-9ATifwbCHJKSdas" title="YouTube video player"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 allowfullscreen></iframe>
         </div>
 
         <hr>
+        <h5 class="mt-4 mb-2 content-translate-font mt-3 mb-1">{{ __('school/public-speaking/public-speaking.thewinner')  }} {{ __('school/public-speaking/public-speaking.n1') }}{{ __('school/public-speaking/public-speaking.n2') }} {{ __('school/public-speaking/public-speaking.in') }} 2024 ({{ __('school/public-speaking/public-speaking.Batch') }} 9)</h5>
+        <div class="row">
+            <div class="col-4 height-200 content-translate-font">
+                <div class="position-relative d-flex justify-content-center align-items-center cursor-pointer" onclick="setVideoSource(this)">
+                    <img src="{{ asset('asset/img/biu/speaking/youtube-logo.png') }}" width="20%" class="position-absolute" alt="">
+                    <img class="w-100 rounded shadow-sm" src="https://img.youtube.com/vi/avZyd_SInnI/hqdefault.jpg" alt="YouTube Video Thumbnail" data-src="https://www.youtube.com/embed/avZyd_SInnI?si=-9ATifwbCHJKSdas">
+
+                </div>
+
+                    <p class="goldenrod font-bold">
+                        {{ __('school/public-speaking/public-speaking.place') }} {{ __('school/public-speaking/public-speaking.n1') }}
+                    </p>
+            </div>
+            <div class="col-4 height-200 content-translate-font">
+                <div class="position-relative d-flex justify-content-center align-items-center cursor-pointer" onclick="setVideoSource(this)">
+                    <img src="{{ asset('asset/img/biu/speaking/youtube-logo.png') }}" width="20%" class="position-absolute" alt="">
+                    <img class="w-100 rounded shadow-sm" src="https://img.youtube.com/vi/S3mPS60DLz4/hqdefault.jpg" alt="YouTube Video Thumbnail" data-src="https://www.youtube.com/embed/S3mPS60DLz4?si=M4vIxkKLJrMXuKnS">
+
+                </div>
+
+                    <p class="goldenrod font-bold">
+                        {{ __('school/public-speaking/public-speaking.place') }} {{ __('school/public-speaking/public-speaking.n2') }}
+                    </p>
+            </div>
+            <div class="col-4 height-200 content-translate-font">
+                <div class="position-relative d-flex justify-content-center align-items-center cursor-pointer" onclick="setVideoSource(this)">
+                    <img src="{{ asset('asset/img/biu/speaking/youtube-logo.png') }}" width="20%" class="position-absolute" alt="">
+                    <img class="w-100 rounded shadow-sm" src="https://img.youtube.com/vi/h4nTSMKPd2w/hqdefault.jpg" alt="YouTube Video Thumbnail" data-src="https://www.youtube.com/embed/h4nTSMKPd2w?si=umKXzdWC7abr9wvM">
+
+                </div>
+
+                    <p class="goldenrod font-bold">
+                        {{ __('school/public-speaking/public-speaking.place') }} {{ __('school/public-speaking/public-speaking.n3') }}
+                    </p>
+            </div>
+        </div>
+        <hr class="border border-success border-3">
+
         <h5 class="mt-4 mb-2 content-translate-font mt-3 mb-1">{{ __('school/public-speaking/public-speaking.thewinner')  }} {{ __('school/public-speaking/public-speaking.n1') }}{{ __('school/public-speaking/public-speaking.n2') }} {{ __('school/public-speaking/public-speaking.in') }} 2023 ({{ __('school/public-speaking/public-speaking.Batch') }} 8)</h5>
         <div class="row">
             <div class="col-4 height-200 content-translate-font">
