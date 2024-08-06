5c223879c844a6ae360276c8f4c1ba726950624c
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-08-06 15:24:04 +0700

    Update construction.blade.php

--- resources/views/web/client/beltei_university/construction.blade.php
+++ resources/views/web/client/beltei_university/construction.blade.php
@@ -7,6 +7,6 @@
         <img class="w-80" src="{{ asset("asset/img/underconstruction.jpg") }}" alt="">
     </div>
 
-    @include('web.client.beltei_univeristy.layout.share.news')
+    @include('web.client.beltei_university.layout.share.news')
     
 @endsection
