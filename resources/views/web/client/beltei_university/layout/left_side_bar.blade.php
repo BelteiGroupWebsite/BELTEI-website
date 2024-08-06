c132f746a1451cb5c35b9b6c3c49c3fae17a6532
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-08-06 11:50:56 +0700

    Update left_side_bar.blade.php

--- resources/views/web/client/beltei_university/layout/left_side_bar.blade.php
+++ resources/views/web/client/beltei_university/layout/left_side_bar.blade.php
@@ -85,9 +85,9 @@
             <h6 class="text-uppercase text-center py-2 bg-header-color gold ">{{ __('beltei_university/app.F') }}</h6>
         </a>
         <a href="{{ route('beltei_university.admission.register') }}" class="list-group-item text-white">{{ __('beltei_university/app.F1') }}</a>
-        <a href="{{ route('beltei_university.admission.price') }}" class="list-group-item text-white">{{ __('beltei_university/app.F2') }}</a>
+        <a href="{{ route('beltei_university.construction') }}" class="list-group-item text-white">{{ __('beltei_university/app.F2') }}</a>
         <a href="{{ route('beltei_university.construction') }}" class="list-group-item text-white">{{ __('beltei_university/app.F3') }}</a>
-        <a href="{{ route('beltei_university.construction') }}" class="list-group-item text-white">{{ __('beltei_university/app.F4') }}</a>
+        <a href="{{ route('beltei_university.admission.register') }}" class="list-group-item text-white">{{ __('beltei_university/app.F4') }}</a>
         <a href="{{ route('beltei_university.construction') }}" class="list-group-item text-white">{{ __('beltei_university/app.F5') }}</a>
     </ul>
     <ul class="text-capitalize list-group f14">
