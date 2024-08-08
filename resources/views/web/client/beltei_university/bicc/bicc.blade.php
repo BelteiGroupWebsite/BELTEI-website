e4d8078713c1afdd06dbd1e682a7b0ac50cedee4
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-08-08 15:53:17 +0700

    update bicc

--- resources/views/web/client/beltei_university/bicc/bicc.blade.php
+++ resources/views/web/client/beltei_university/bicc/bicc.blade.php
@@ -8,37 +8,31 @@
         </div>
         <hr>
 
-        {{-- Hot News  --}}
-        <a href="{{ route('beltei_university.news.detail', $bicc_news->first()->id) }}" class="card card-body d-flex">
-            {{-- <a href="{{ route('school.news.bacll-batch18') }}" class="card card-body d-flex"> --}}
-            <div class="overflow-hidden">
-                @if ($bicc_news->first()->image)
-                    @foreach (array_slice(explode(',', $bicc_news->first()->image), 0, 1) as $newsImg)
-                        <img class="rounded shadow-sm width-100 w-100" loading="lazy"
-                            src="{{ asset('uploaded/university/news/images/' . $bicc_news->first()->id . '/' . $newsImg) }}"
-                            alt="News Image">
-                    @endforeach
-                @endif
-                {{-- <img class="w-100" src="{{ asset('asset/img/school/dailyNews/1/1.jpg') }}" alt=""> --}}
-            </div>
-            <div class="p-3">
-                <h6 class="Muol-Light line-height-15">
-                    @foreach ($bicc_news->first()->newsDetail as $item)
-                        @if ($item->language_id == 1)
-                            <div onclick="toggleDescription(this)" class="cursor-pointer "
-                                data-fulltext="{{ $item->header }}">
-                                {{ Str::limit($item->header, 200) }}
-                            </div>
-                        @endif
-                    @endforeach
-                </h6>
-            </div>
-        </a>
-
+        <div class="text-center">
+            <img src="{{ asset('asset/img/university/bicc/BICC.png') }}" class="w-75" alt="">
+        </div>
 
         <div class="row p-0 m-0">
 
-            @foreach ($bicc_news as $news)
+            <ul>
+                @foreach ($bicc_news as $news)
+                <li>
+                    <a href="{{ route('beltei_university.news.detail', $news->id) }}">
+                        <h6 class="content-translate-font f12 line-height-15">
+                            @foreach ($news->newsDetail as $item)
+                                @if ($item->language_id == 1)
+                                    <div onclick="toggleDescription(this)" class="cursor-pointer "
+                                        data-fulltext="{{ $item->header }}">
+                                        {{ Str::limit($item->header, 70) }}
+                                    </div>
+                                @endif
+                            @endforeach
+                        </h6>
+                    </a>
+                </li>
+                @endforeach
+            </ul>
+            {{-- @foreach ($bicc_news as $news)
                 <a href="{{ route('beltei_university.news.detail', $news->id) }}"
                     class="col-6 row align-items-center py-2 border m-0">
                     <div class=" col-6 overflow-hidden">
@@ -69,7 +63,7 @@
 
                     </div>
                 </a>
-            @endforeach
+            @endforeach --}}
 
         </div>
 
