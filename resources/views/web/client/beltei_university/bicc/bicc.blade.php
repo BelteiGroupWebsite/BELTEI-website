15dd44152df62180764408a12fc00aa1109fd4d8
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-08-08 17:09:02 +0700

    Update bicc.blade.php

--- resources/views/web/client/beltei_university/bicc/bicc.blade.php
+++ resources/views/web/client/beltei_university/bicc/bicc.blade.php
@@ -53,6 +53,33 @@
                         <h6>2- BELTEI International Collaboration Conference Research Forum</h6>
                     </td>
                 </tr>
+                @foreach ($biccr_news as $news)
+                    <tr>
+                        <td class="ps-4 py-2">
+                            <a href="{{ route('beltei_university.news.detail', $news->id) }}">
+                                <h6 class="content-translate-font line-height-15">
+                                    @foreach ($news->newsDetail as $item)
+                                        @if(app()->getLocale() == "kh")
+                                            @if ($item->language_id == 1)
+                                                <div onclick="toggleDescription(this)" class="cursor-pointer "
+                                                    data-fulltext="{{ $item->header }}">
+                                                    - {{ Str::limit($item->header, 70) }}
+                                                </div>
+                                            @endif
+                                        @else
+                                            @if ($item->language_id == 2)
+                                                <div onclick="toggleDescription(this)" class="cursor-pointer "
+                                                    data-fulltext="{{ $item->header }}">
+                                                    - {{ Str::limit($item->header, 70) }}
+                                                </div>
+                                            @endif
+                                        @endif
+                                    @endforeach
+                                </h6>
+                            </a>
+                        </td>
+                    </tr>
+                @endforeach
             </table>
             {{-- @foreach ($bicc_news as $news)
                 <a href="{{ route('beltei_university.news.detail', $news->id) }}"
