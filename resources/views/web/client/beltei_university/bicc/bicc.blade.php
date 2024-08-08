c50e189eaf9ec508f3f4529b36fd98e2e5415e86
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-08-08 16:04:20 +0700

    Update bicc.blade.php

--- resources/views/web/client/beltei_university/bicc/bicc.blade.php
+++ resources/views/web/client/beltei_university/bicc/bicc.blade.php
@@ -23,7 +23,7 @@
                 </tr>
                 @foreach ($bicc_news as $news)
                     <tr>
-                        <td class="ps-2">
+                        <td class="ps-4">
                             <a href="{{ route('beltei_university.news.detail', $news->id) }}">
                                 <h6 class="content-translate-font line-height-15">
                                     @foreach ($news->newsDetail as $item)
