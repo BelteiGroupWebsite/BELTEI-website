d020d441325c591af7e23a019ed5e1cd18fbc20e
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-08-08 17:31:20 +0700

    Update bicc.blade.php

--- resources/views/web/client/beltei_university/bicc/bicc.blade.php
+++ resources/views/web/client/beltei_university/bicc/bicc.blade.php
@@ -53,7 +53,7 @@
                         <h6>2- BELTEI International Collaboration Conference Research Forum</h6>
                     </td>
                 </tr>
-                @foreach ($biccr_news as $news)
+                @foreach ($biccr_news->reverse() as $news)
                     <tr>
                         <td class="ps-4 py-2">
                             <a href="{{ route('beltei_university.news.detail', $news->id) }}">
