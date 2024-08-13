6d68d296224ee2e18eaef8d94cbe8d130413a657
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-08-13 16:11:42 +0700

    admission

---
+++ resources/lang/en/beltei_university/admission/price.php
@@ -0,0 +1,139 @@
+<?php
+
+return [
+
+    'header' => "",
+
+    'description' => '
+    
+<div>
+ <h4 class = "title-translate-font"> 1. Tuition Fee for National Program  </h4>
+ <table class = "table table-hover table-bordered">
+ <tr class = "bg-primary title-translate-font text-white">
+ <th> Program </th>
+ <th> Semester </th>
+ <th> Year </th>
+ </tr>
+ <tr>
+ <td> Associate Degree </td>
+ <td> $ 380 </td>
+ <td> $ 700 </td>
+ </tr>
+ <tr>
+ <td> Bachelor Degree </td>
+ <td> $ 380 </td>
+ <td> $ 700 </td>
+ </tr>
+ <tr>
+ <td> Master\'s Degree </td>
+ <td> $ 800 </td>
+ <td> $ 1,500 </td>
+ </tr>
+ <tr>
+ <td> PhD </td>
+ <td> $ 1,300 </td>
+ <td> $ 2,500 </td>
+ </tr>
+ </table>
+ </div>
+
+ <div>
+ <h4 class = "title-translate-font mt-4"> 2. Tuition Fee for International Program </h4>
+ <table class = "table table-hover table-bordered">
+ <tr class = "bg-primary title-translate-font text-white">
+ <th> Program </th>
+ <th> Semester </th>
+ <th> Year </th>
+ </tr>
+ <tr>
+ <td> Associate Degree </td>
+ <td> $ 550 </td>
+ <td> $ 1,000 </td>
+ </tr>
+ <tr>
+ <td> Bachelor Degree </td>
+ <td> $ 550 </td>
+ <td> $ 1,000 </td>
+ </tr>
+ <tr>
+ <td> Master\'s Degree </td>
+ <td> $ 800 </td>
+ <td> $ 1,500 </td>
+ </tr>
+ <tr>
+ <td> PhD </td>
+ <td> $ 1,300 </td>
+ <td> $ 2,500 </td>
+ </tr>
+ </table>
+ </div>
+
+ <div>
+ <h4 class = "title-translate-font mt-4"> 3. Tuition Fee for International Program (Civil Aviation) </h4>
+ <table class = "table table-hover table-bordered">
+ <tr class = "bg-primary title-translate-font text-white">
+ <th> Program </th>
+ <th> Semester </th>
+ <th> Year </th>
+ </tr>
+ <tr>
+ <td> Associate Degree </td>
+ <td> $ 1,100 </td>
+ <td> $ 2,000 </td>
+ </tr>
+ <tr>
+ <td> Bachelor Degree </td>
+ <td> $ 1,600 </td>
+ <td> $ 3,000 </td>
+ </tr>
+ <tr>
+ <td> Master\'s Degree </td>
+ <td> $ 2,600 </td>
+ <td> $ 5,000 </td>
+ </tr>
+ </table>
+ </div>
+
+ <div>
+ <h4 class = "title-translate-font mt-4"> DAY AND TIME </h4>
+ <table class = "table table-hover table-bordered">
+ <tr class = "bg-primary title-translate-font text-white">
+ <th> Monday – Friday </th>
+ <th> Time </th>
+ </tr>
+ <tr>
+ <td> Morning </td>
+ <td> 08:00AM – 11:15AM </td>
+ </tr>
+ <tr>
+ <td> Afternoon </td>
+ <td> 02:00PM – 05:15PM </td>
+ </tr>
+ <tr>
+ <td> Evening </td>
+ <td> 05:30PM – 08:30PM </td>
+ </tr>
+ </table>
+ <table class = "table table-hover table-bordered">
+ <tr class = "bg-primary title-translate-font text-white">
+ <th> Saturday – Sunday </th>
+ <th> Time </th>
+ </tr>
+ <tr>
+ <td> Saturday </td>
+ <td> 
+ 08:00AM – 11:15AM <br>
+ 01:30PM – 05:30PM
+ </td>
+ </tr>
+ <tr>
+ <td> Sunday </td>
+ <td> 02:00PM – 05:15PM <br> 01:30PM – 05:30PM</td>
+ </tr>
+ </table>
+ </div>
+ 
+ 
+    '
+    
+];
---
+++ resources/lang/kh/beltei_university/admission/price.php
@@ -0,0 +1,99 @@
+<?php
+
+return [
+
+    'header' => "",
+
+    'description' => '
+    
+        <div>
+        <h4 class="title-translate-font">១. តម្លៃសិក្សាកម្មវិធីជា ភាសាជាតិ</h4>
+        <table class="table table-hover table-bordered">
+            <tr class="bg-primary title-translate-font text-white">
+                <th>កម្រិតសិក្សា</th>
+                <th>ឆមាស</th>
+                <th>ឆ្នាំ</th>
+            </tr>
+            <tr>
+                <td>ថ្នាក់បរិញ្ញាបត្ររង</td>
+                <td>$ 380</td>
+                <td>$ 700</td>
+            </tr>
+            <tr>
+                <td>ថ្នាក់បរិញ្ញាបត្រ</td>
+                <td>$ 380</td>
+                <td>$ 700</td>
+            </tr>
+            <tr>
+                <td>ថ្នាក់បរិញ្ញាបត្រជាន់ខ្ពស់</td>
+                <td>$ 800</td>
+                <td>$ 1,500</td>
+            </tr>
+            <tr>
+                <td>ថ្នាក់បណ្ឌិត</td>
+                <td>$ 1,300</td>
+                <td>$ 2,500</td>
+            </tr>
+        </table>
+    </div>
+
+    <div>
+        <h4 class="title-translate-font mt-4">២. តម្លៃសិក្សាកម្មវិធីអន្តរជាតិ</h4>
+        <table class="table table-hover table-bordered">
+            <tr class="bg-primary title-translate-font text-white">
+                <th>កម្រិតសិក្សា</th>
+                <th>ឆមាស</th>
+                <th>ឆ្នាំ</th>
+            </tr>
+            <tr>
+                <td>ថ្នាក់បរិញ្ញាបត្ររង</td>
+                <td>$ 550</td>
+                <td>$ 1,000</td>
+            </tr>
+            <tr>
+                <td>ថ្នាក់បរិញ្ញាបត្រ</td>
+                <td>$ 550</td>
+                <td>$ 1,000</td>
+            </tr>
+            <tr>
+                <td>ថ្នាក់បរិញ្ញាបត្រជាន់ខ្ពស់</td>
+                <td>$ 800</td>
+                <td>$ 1,500</td>
+            </tr>
+            <tr>
+                <td>ថ្នាក់បណ្ឌិត</td>
+                <td>$ 1,300</td>
+                <td>$ 2,500</td>
+            </tr>
+        </table>
+    </div>
+
+    <div>
+        <h4 class="title-translate-font mt-4">៣. តម្លៃសិក្សាកម្មវិធីអន្តរជាតិ (អាកាសចរស៊ីវិល)</h4>
+        <table class="table table-hover table-bordered">
+            <tr class="bg-primary title-translate-font text-white">
+                <th>កម្រិតសិក្សា</th>
+                <th>ឆមាស</th>
+                <th>ឆ្នាំ</th>
+            </tr>
+            <tr>
+                <td>ថ្នាក់បរិញ្ញាបត្ររង</td>
+                <td>$ 1,100</td>
+                <td>$ 2,000</td>
+            </tr>
+            <tr>
+                <td>ថ្នាក់បរិញ្ញាបត្រ</td>
+                <td>$ 1,600</td>
+                <td>$ 3,000</td>
+            </tr>
+            <tr>
+                <td>ថ្នាក់បរិញ្ញាបត្រជាន់ខ្ពស់</td>
+                <td>$ 2,600</td>
+                <td>$ 5,000</td>
+            </tr>
+        </table>
+    </div>
+    
+    '
+    
+];
