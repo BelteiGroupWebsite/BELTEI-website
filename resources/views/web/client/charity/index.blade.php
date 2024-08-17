d92ffd94b0cb4e8d38e907c75c3947e5f19eecf7
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-08-17 14:13:06 +0700

    Update index.blade.php

--- resources/views/web/client/charity/index.blade.php
+++ resources/views/web/client/charity/index.blade.php
@@ -1,10 +1,58 @@
 @extends('web.client.charity.layout.app')
 
 @section('content')
+
+<style>
+    .welcomeText {
+        -webkit-text-stroke: 1px rgba(255, 255, 255, 0.86);
+        text-shadow: 1px 1px 3px black;
+    }
+
+    .line-1 {
+        position: relative;
+        /* top: 50%;   */
+        width: 24em;
+        margin: 0 auto;
+        border-right: 2px solid rgba(255, 255, 255, .75);
+        /* font-size: 180%; */
+        text-align: center;
+        white-space: nowrap;
+        overflow: hidden;
+        /* transform: translateY(-50%);     */
+    }
+
+    /* Animation */
+    .anim-typewriter {
+        /* background-color: green; */
+        animation: typewriter 4s steps(44) 1s infinite normal both,
+            blinkTextCursor 500ms steps(44) infinite normal;
+    }
+
+    @keyframes typewriter {
+        from {
+            width: 0;
+        }
+
+        to {
+            width: 100%;
+        }
+    }
+
+    @keyframes blinkTextCursor {
+        from {
+            border-right-color: rgba(255, 255, 255, .75);
+        }
+
+        to {
+            border-right-color: transparent;
+        }
+    }
+</style>
+
 <div class="text-center">
-    
+
     <h3 class="title-translate-font text-primary welcomeText line-1 anim-typewriter py-2" style="">សូមស្វាគមន៍​
-        មកកាន់ ប៊ែលធី សំណង់</h3>
+        មកកាន់ មូលនិធិសប្បុរសធម៍ប៊ែលធី</h3>
     {{-- <p class="line-1 anim-typewriter">Animation typewriter style using css steps()</p> --}}
     <div>
         <img src="{{ asset('asset/img/construction/facebook.jpg') }}" alt="">
