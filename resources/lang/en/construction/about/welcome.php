418c502ae0aa996e65bf8a408619c924c0bf86dd
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-08-20 16:47:32 +0700

    update tour header

---
+++ resources/lang/en/construction/about/welcome.php
@@ -0,0 +1,15 @@
+<?php 
+
+return [
+    'header' => "សារស្វាគមន៍",
+
+    "description"=> "
+
+<p>I would like to extend my great welcome and thank to Samdech, Your Excellency, Lok Oknha, Lok Chum Teav, Ladies, Gentlemen, and all the clients who have trusted and chosen BELTEI Construction for selling and buying your real-estates and constructing your buildings. BELTEI Construction was established in 1991 and was a private family-owned company which provided services in constructing flats for sales, landowner joint-venture investments, and all types of real-estates.</p>
+<p>Having been growing continuously, this private family-run business became BELTEI Construction with its own office and staff operating under the license of the Ministry of Land management, Urban planning, and Construction&rsquo;s No. 584នសគប/09ស, date April 24, 2009. BELTEI Construction has dedicated teams of professional engineers, architects, construction foremen, and construction workers who are all skillful Cambodians &mdash;from project studies to construction processes such as foundations, material installations, utility system constructions, brick-works, floorings, wall plastering and paintings, and other steps making all construction projects go smoothly with result of fairness, quality, and reasonable price.</p>
+<p>BELTEI Construction has wide-range of experience in constructing villas, flats, apartments, schools, malls, and as well as factories by equipping with latest technologies and modern techniques in order to improve the architectural designs and save times and cost of expenses. BELTEI Construction has so far collaborated with many leading local and international construction companies for the purpose of supplying the best quality of construction materials and furniture, which makes BELTEI Construction the first choice for all clients in all real-estates and constructions sector.</p>
+<p>Finally, I utterly believe that by taking BELTEI Construction&rsquo;s service will make building of your dreams come true. Please come to visit us for detailed information, consultancy on real-estates, and constructions upon your requirements at BELTEI Construction office at BELTEI International University&rsquo;s 4<sup>th</sup> floor at business hour.</p>
+<p>I would like to wish Samdech, Your Excellency, Lok Oknha, Lok Chum Teav, Ladies, Gentlemen, and all the clients healthy and successful in all kinds of works.</p>
+    
+    ",
+];
--- resources/views/web/client/contruction/about/welcome.blade.php
+++ resources/views/web/client/contruction/about/welcome.blade.php
@@ -1,17 +1,25 @@
 @extends('web.client.contruction.layout.app')
 
 @section('content')
-    <h2 class="title-translate-font my-3 underline">{{ __('beltei_tours_travel/about/welcome.header') }}</h2>
+
+    <style>
+        .content-container p{
+            padding-top: 1rem;
+            text-indent: 1rem;
+        }
+    </style>
+
+    <h2 class="title-translate-font my-3 underline">{{ __('construction/about/welcome.header') }}</h2>
 
 
 
 
     <div class="text-start">
-        <div class="content-translate-font p-lg-4 ul-none-style">
+        <div class="content-translate-font p-lg-4 ul-none-style content-container">
             <img class="float-start rounded shadow-sm p-3"
                 src="https://www.beltei.edu.kh/khm/images/beltei_pic/H.E_Dr_Ly_chheng_beltei.jpg" alt="">
             <div class="text-justify">
-                {!! __('beltei_tours_travel/about/welcome.welcome') !!}
+                {!! __('construction/about/welcome.welcome') !!}
             </div>
         </div>
     </div>
