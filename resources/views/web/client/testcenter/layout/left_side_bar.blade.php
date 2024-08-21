983e8339aba605d09d8cf4a96c4efb0b4a42ba12
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-08-21 16:17:41 +0700

    Hello world

--- resources/views/web/client/testcenter/layout/left_side_bar.blade.php
+++ resources/views/web/client/testcenter/layout/left_side_bar.blade.php
@@ -17,24 +17,6 @@
         <a href="{{ route('school.about.vision') }}" class="list-group-item text-white">{{ __('testcenter/app.B3') }}</a>
         <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('testcenter/app.B4') }}</a>
     </ul>
-    <ul class="text-capitalize list-group f14 unhover-padding">
-        <a href="{{ route('school.') }}">
-            <h6 class="text-uppercase text-center py-2 bg-header-color gold ">{{ __('testcenter/app.C') }}</h6>
-        </a>
-        <a href="https://www.gre.ac.uk/" class=" text-white" target="_blank">
-            <img class="w-100 justify-content-center list-group-item" src="{{ asset('asset/img/testcenter/TOEFL.png') }}" alt="">
-        </a>
-        <a href="https://ctc.ac.uk/" class=" text-white" target="_blank">
-            <img class="w-100 justify-content-center list-group-item" src="{{ asset('asset/img/testcenter/GRE.png') }}" alt="">
-        </a>
-        <a href="https://www.yorksj.ac.uk/" class=" text-white" target="_blank">
-            <img class="w-100 justify-content-center list-group-item" src="{{ asset('asset/img/testcenter/IELTS.png') }}" alt="">
-        </a>
-        <a href="https://www.regents.ac.uk/" class=" text-white" target="_blank">
-            <img class="w-100 justify-content-center list-group-item" src="{{ asset('asset/img/testcenter/SELT.png') }}" alt="">
-        </a>
-
-    </ul>
 
     <ul class="p-0 m-0  list-group ">
 
@@ -42,27 +24,22 @@
     
             <li class="list-group-item p-0">
                 <a href="https://www.smu.ac.kr/eng1/index.do" target="_blank">
-                    <img class="w-100 justify-content-center" src="{{ asset('asset/img/university/app/rightsidebar/korea/1.png') }}" alt="">
+                    <img class="w-100 justify-content-center" src="{{ asset('asset/img/testcenter/TOEFL.png') }}" alt="">
                 </a>
             </li>
             <li class="list-group-item p-0">
                 <a href="https://en.shinhan.ac.kr/en/index.do" target="_blank">
-                    <img class="w-100 justify-content-center" src="{{ asset('asset/img/university/app/rightsidebar/korea/2.png') }}" alt="">
+                    <img class="w-100 justify-content-center" src="{{ asset('asset/img/testcenter/GRE.png') }}" alt="">
                 </a>
             </li>
             <li class="list-group-item p-0">
                 <a href="https://www.sunykorea.ac.kr/en/" target="_blank">
-                    <img class="w-100 justify-content-center" src="{{ asset('asset/img/university/app/rightsidebar/korea/3.png') }}" alt="">
+                    <img class="w-100 justify-content-center" src="{{ asset('asset/img/testcenter/IELTS.png') }}" alt="">
                 </a>
             </li>
             <li class="list-group-item p-0">
                 <a href="https://global.kduniv.ac.kr/global/index.php" target="_blank">
-                    <img class="w-100 justify-content-center" src="{{ asset('asset/img/university/app/rightsidebar/korea/4.png') }}" alt="">
-                </a>
-            </li>
-            <li class="list-group-item p-0">
-                <a href="https://en.sejong.ac.kr/eng/index.do" target="_blank">
-                    <img class="w-100 justify-content-center" src="{{ asset('asset/img/university/app/rightsidebar/korea/5.png') }}" alt="">
+                    <img class="w-100 justify-content-center" src="{{ asset('asset/img/testcenter/SELT.png') }}" alt="">
                 </a>
             </li>
 
