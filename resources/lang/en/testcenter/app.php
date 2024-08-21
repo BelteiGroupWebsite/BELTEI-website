f417cfbbb201871c60e578d2686513e5e575eea9
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-08-21 15:32:25 +0700

    Hello world

---
+++ resources/lang/en/testcenter/app.php
@@ -0,0 +1,13 @@
+<?php
+
+return [    
+    "A" => "About International Testing",
+    "B" => "Service Available",
+    "B1" => "TOEFL International Test",
+    "B2" => "GE International Test GRE",
+    "B3" => "IELTS International Test",
+    "B4" => "SEE International Test",
+
+    "C" => "Exam Registration"
+
+];
--- resources/views/web/client/testcenter/layout/left_side_bar.blade.php
+++ resources/views/web/client/testcenter/layout/left_side_bar.blade.php
@@ -1,41 +1,66 @@
 <div class="sidebar max-width-250 w-100 shadow-sm p-0 m-0 content-translate-font px-1">
     <ul class="text-capitalize list-group f14">
         <a href="{{ route('school.') }}">
-            <h6 class="text-uppercase text-center py-2 bg-header-color gold ">{{ __('school/app.A') }}</h6>
+            <h6 class="text-uppercase text-center py-2 bg-header-color gold ">{{ __('testcenter/app.A') }}</h6>
         </a>
         <a href="{{ route('testcenter.about.welcome') }}" class="list-group-item text-white">{{ __('school/app.A1') }}</a>
         <a href="{{ route('school.about.purpose') }}" class="list-group-item text-white">{{ __('school/app.A2') }}</a>
         <a href="{{ route('school.about.vision') }}" class="list-group-item text-white">{{ __('school/app.A3') }}</a>
         <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('school/app.A4') }}</a>
-        <a href="{{ asset('asset/img/school/organization_chart.jpg') }}" target="blank" class="list-group-item text-white">{{ __('school/app.A5') }}</a>
-        <a href="{{ route('school.about.whybeltei') }}" class="list-group-item text-white">{{ __('school/app.A6') }}</a>
     </ul>
     <ul class="text-capitalize list-group f14">
         <a href="{{ route('school.') }}">
-            <h6 class="text-uppercase text-center py-2 bg-header-color gold ">{{ __('school/app.B') }}</h6>
+            <h6 class="text-uppercase text-center py-2 bg-header-color gold ">{{ __('testcenter/app.B') }}</h6>
         </a>
-        <a href="{{ route('school.mainprogram.khmer') }}" class="list-group-item text-white">{{ __('school/app.B1') }}</a>
-        <a href="{{ route('school.about.purpose') }}" class="list-group-item text-white">{{ __('school/app.B2') }}</a>
-        <a href="{{ route('school.about.vision') }}" class="list-group-item text-white">{{ __('school/app.B3') }}</a>
-        <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('school/app.B4') }}</a>
-        <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('school/app.B5') }}</a>
+        <a href="{{ route('school.mainprogram.khmer') }}" class="list-group-item text-white">{{ __('testcenter/app.B1') }}</a>
+        <a href="{{ route('school.about.purpose') }}" class="list-group-item text-white">{{ __('testcenter/app.B2') }}</a>
+        <a href="{{ route('school.about.vision') }}" class="list-group-item text-white">{{ __('testcenter/app.B3') }}</a>
+        <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('testcenter/app.B4') }}</a>
     </ul>
-    <ul class="text-capitalize list-group f14">
+    {{-- <ul class="text-capitalize list-group f14">
         <a href="{{ route('school.') }}">
-            <h6 class="text-uppercase text-center py-2 bg-header-color gold ">{{ __('school/app.C') }}</h6>
+            <h6 class="text-uppercase text-center py-2 bg-header-color gold ">{{ __('testcenter/app.C') }}</h6>
         </a>
-        <a href="{{ route('school.about.welcome') }}" class="list-group-item text-white">{{ __('school/app.C1') }}</a>
-        <a href="{{ route('school.about.purpose') }}" class="list-group-item text-white">{{ __('school/app.C2') }}</a>
-        <a href="{{ route('school.about.vision') }}" class="list-group-item text-white">{{ __('school/app.C3') }}</a>
-        <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('school/app.C4') }}</a>
-        <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('school/app.C5') }}</a>
-        <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('school/app.C6') }}</a>
-        <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('school/app.C7') }}</a>
-        <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('school/app.C8') }}</a>
-        <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('school/app.C9') }}</a>
-        <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('school/app.C10') }}</a>
-        <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('school/app.C11') }}</a>
-        <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('school/app.C12') }}</a>
-    </ul>
+        <a href="{{ route('school.mainprogram.khmer') }}" class="list-group-item text-white">{{ __('testcenter/app.C1') }}</a>
+        <a href="{{ route('school.about.purpose') }}" class="list-group-item text-white">{{ __('testcenter/app.C2') }}</a>
+        <a href="{{ route('school.about.vision') }}" class="list-group-item text-white">{{ __('testcenter/app.C3') }}</a>
+        <a href="{{ route('school.about.history') }}" class="list-group-item text-white">{{ __('testcenter/app.C4') }}</a>
+    </ul> --}}
 
+    <ul class="p-0 m-0  list-group ">
+
+        <li class="list-group-item d-flex justify-content-center">
+    
+            <div style="border:inset 7px ; border-radius: 15px ; border-color: #03a5c2" class="w-100 overflow-hidden border-blue">
+                <a href="{{ route('school.') }}">
+                    <h6 class="text-uppercase text-center py-2 bg-header-color gold ">{{ __('testcenter/app.C') }}</h6>
+                </a>
+    
+                <ul class="list-group right-bar-ul">
+                    <li class="list-group-item p-0">
+                        <a href="https://www.gre.ac.uk/" target="_blank">
+                            <img class="w-100 justify-content-center" src="{{ asset('asset/img/testcenter/TOEFL.png') }}" alt="">
+                        </a>
+                    </li>
+                    <li class="list-group-item p-0">
+                        <a href="https://ctc.ac.uk/" target="_blank">
+                            <img class="w-100 justify-content-center" src="{{ asset('asset/img/testcenter/GRE.png') }}" alt="">
+                        </a>
+                    </li>
+                    <li class="list-group-item p-0">
+                        <a href="https://www.yorksj.ac.uk/" target="_blank">
+                            <img class="w-100 justify-content-center" src="{{ asset('asset/img/testcenter/IELTS.png') }}" alt="">
+                        </a>
+                    </li>
+                    <li class="list-group-item p-0">
+                        <a href="https://www.regents.ac.uk/" target="_blank">
+                            <img class="w-100 justify-content-center" src="{{ asset('asset/img/testcenter/SELT.png') }}" alt="">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </li>
+    
+    </ul>
+    
 </div>
