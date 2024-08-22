503a8c7d0946928b1b1b47d476eeb7460ac43e7e
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-08-22 09:01:22 +0700

    Hello world

--- resources/lang/en/testcenter/about/vision.php
+++ resources/lang/en/testcenter/about/vision.php
@@ -5,15 +5,15 @@ return [
 
  "description" => "
 
- <h3>Vision</h3>
+ <h4 class='pt-2'>Vision</h4>
 <p> BELTEI International Test Center & nbsp; will be the leading test center in Cambodia in accordance with national and international standards that are recognized worldwide in all types of international examinations. In addition, BELTEI International Test is an excellent bridge for national and international students to prepare for further study or work at home and abroad, and this center is a mirror to reflect the ability and experience to An effective world in various fields, especially education. </p>
-<h3>Mission</h3>
+<h4 class='pt-2'>Mission</h4>
 <p> 1. Disseminate, consult and guide the completion of international test registration forms to national and international students effectively. </p>
 <p> 2. Provide exam room equipped with enough materials to provide convenience and comfort, as well as ensure accuracy to candidates during the exam. </p>
 <p> 3. Provide candidates with diplomas and medals that are officially recognized by international testing institutions as partners and have the opportunity to participate in competitions on various international stages. </p>
 <p> 4. Provide opportunities to apply for scholarships or jobs for candidates who score high on international tests. </p>
 <p> 5. Continue to cooperate with existing partners and seek additional partners to expand the scope of BELTEI International Testing Center. </p>
-<h3>Goal</h3>
+<h4 class='pt-2'>Goal</h4>
 <p> BELTEI International Testing Center has a clear vision and mission of providing consulting and examination services. The language is recognized in accordance with national and international standards and has the opportunity to get good jobs and scholarships to study at famous universities around the world, which can contribute to the development of a more prosperous society in the near future. / p>
 
 
