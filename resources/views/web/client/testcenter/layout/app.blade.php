cc2f4836fcd703edc732a005fa38fdc1a0ad3c4c
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-08-22 09:53:25 +0700

    TEST CENTER

--- resources/views/web/client/testcenter/layout/app.blade.php
+++ resources/views/web/client/testcenter/layout/app.blade.php
@@ -46,7 +46,7 @@
             background-color: whitesmoke;
             width: 100px;
             height: 100px;
-            font-size: 26px;
+            font-size: 20px;
             color: var(--bg-header-color);
             border: 3px solid var(--bg-header-color);
         }
