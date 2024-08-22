48c21018e2109b192de982aa2fd21c7ce64408bb
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-08-22 10:33:41 +0700

    TEST CENTER

--- resources/views/web/client/share/visitor.blade.php
+++ resources/views/web/client/share/visitor.blade.php
@@ -3,7 +3,7 @@
         <h6 class="text-uppercase text-center py-2 bg-header-color gold ">Visitors : {{ $visitor->count() }}</h6>
         <table class="table content-translate-font text-white">
             @foreach ($countries as $country)
-                @if($country->visitors->count() > 10)
+                @if($country->visitors->count() > 50)
                 <tr>
                     <td>
                         {{ $country->region ." / ". $country->name }}
