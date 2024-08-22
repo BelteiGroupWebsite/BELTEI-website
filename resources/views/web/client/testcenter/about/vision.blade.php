295099fff08403e3aad44b543796f877c0465aa7
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-08-22 08:54:27 +0700

    Hello world

--- resources/views/web/client/testcenter/about/vision.blade.php
+++ resources/views/web/client/testcenter/about/vision.blade.php
@@ -7,7 +7,7 @@
 
         <h2 class="title-translate-font my-3 underline">{{ __('testcenter/about/vision.header') }}</h2>
 
-        <div class="text-justify">
+        <div class="text-justify content-translate-font">
             {!! __('testcenter/about/vision.description') !!}
         </div>
 
