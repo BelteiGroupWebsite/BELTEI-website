db46b7a97cf1f39c1fdf2d102329f75367714342
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-08-22 08:53:17 +0700

    Hello world

--- resources/views/web/client/testcenter/about/purpose.blade.php
+++ resources/views/web/client/testcenter/about/purpose.blade.php
@@ -6,7 +6,7 @@
 
         <h2 class="title-translate-font my-3 underline">{{ __('testcenter/about/purpose.header') }}</h2>
 
-        <div class="text-start content-translate-font text-justify ">
+        <div class="text-start content-translate-font ">
             {!! __('testcenter/about/purpose.description') !!}
         </div>
 
