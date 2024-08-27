f92221f315a30eba80f182a3ca07d8f1301af092
Chhaty <chhatyhoeurn@gmail.com>
2024-08-27 14:20:29 +0700

    Update grade6.blade.php

--- resources/views/web/client/school/english-speaking/grade6.blade.php
+++ resources/views/web/client/school/english-speaking/grade6.blade.php
@@ -7,11 +7,59 @@
         <hr>
         <div>
             <iframe class="w-100 aspect-ratio-16" id="vd-container"
-                src="https://www.youtube.com/embed/wOE57yFL-HU?si=E3JqVl1M3qINXKao" title="YouTube video player"
+                src="https://www.youtube.com/embed/H96bJnqoefo?si=_i77s6OhhjVYGls0" title="YouTube video player"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 allowfullscreen></iframe>
         </div>
+
+
+        <hr>
+        <h5 class="mt-4 mb-2 content-translate-font mt-3 mb-1">{{ __('school/public-speaking/public-speaking.thewinner') }}
+            {{ __('school/public-speaking/public-speaking.n1') }}{{ __('school/public-speaking/public-speaking.n2') }}
+            {{ __('school/public-speaking/public-speaking.in') }} 2023
+            ({{ __('school/public-speaking/public-speaking.Batch') }} 17)</h5>
+        <div class="row">
+            <div class="col-4 height-200 content-translate-font">
+                <div class="position-relative d-flex justify-content-center align-items-center cursor-pointer" onclick="setVideoSource(this)">
+                    <img src="{{ asset('asset/img/biu/speaking/youtube-logo.png') }}" class="position-absolute" width="20%" alt="">
+                    {{-- <img class="w-100 rounded shadow-sm" src="https://img.youtube.com/vi/RaybuOZlW7k/hqdefault.jpg" alt="YouTube Video Thumbnail" data-src="https://www.youtube.com/embed/RaybuOZlW7k?si=FM-y2LLsRpyxdQ-0"> --}}
+                    <img class="w-100 youtube-thumbnail" src="https://img.youtube.com/vi/H96bJnqoefo/maxresdefault.jpg" alt="YouTube Video Thumbnail" data-src="https://www.youtube.com/embed/H96bJnqoefo?si=_i77s6OhhjVYGls0" />
+                </div>
+
+                <p class="goldenrod font-bold">
+                    {{ __('school\public-speaking\public-speaking.place') }}
+                    {{ __('school/public-speaking/public-speaking.n1') }}
+                </p>
+            </div>
+            <div class="col-4 height-200 content-translate-font">
+                <div class="position-relative d-flex justify-content-center align-items-center cursor-pointer" onclick="setVideoSource(this)">
+                    <img src="{{ asset('asset/img/biu/speaking/youtube-logo.png') }}" class="position-absolute" width="20%" alt="">
+                    {{-- <img class="w-100 rounded shadow-sm" src="https://img.youtube.com/vi/RaybuOZlW7k/hqdefault.jpg" alt="YouTube Video Thumbnail" data-src="https://www.youtube.com/embed/RaybuOZlW7k?si=FM-y2LLsRpyxdQ-0"> --}}
+                    <img class="w-100 youtube-thumbnail" src="https://img.youtube.com/vi/CeXMzJGRGo4/maxresdefault.jpg" alt="YouTube Video Thumbnail" data-src="https://www.youtube.com/embed/CeXMzJGRGo4?si=TKGlKaHNNgpxkrk1" />
+                </div>
+
+                <p class="goldenrod font-bold">
+                    {{ __('school\public-speaking\public-speaking.place') }}
+                    {{ __('school/public-speaking/public-speaking.n2') }}
+                </p>
+            </div>
+            <div class="col-4 height-200 content-translate-font">
+                <div class="position-relative d-flex justify-content-center align-items-center cursor-pointer" onclick="setVideoSource(this)">
+                    <img src="{{ asset('asset/img/biu/speaking/youtube-logo.png') }}" class="position-absolute" width="20%" alt="">
+                    {{-- <img class="w-100 rounded shadow-sm" src="https://img.youtube.com/vi/RaybuOZlW7k/hqdefault.jpg" alt="YouTube Video Thumbnail" data-src="https://www.youtube.com/embed/RaybuOZlW7k?si=FM-y2LLsRpyxdQ-0"> --}}
+                    <img class="w-100 youtube-thumbnail" src="https://img.youtube.com/vi/rr9kVvF9E00/maxresdefault.jpg" alt="YouTube Video Thumbnail" data-src="https://www.youtube.com/embed/rr9kVvF9E00?si=hsa18xNjUigH61o8" />
+                </div>
+
+                <p class="goldenrod font-bold">
+                    {{ __('school\public-speaking\public-speaking.place') }}
+                    {{ __('school/public-speaking/public-speaking.n3') }}
+                </p>
+            </div>
+        </div>
+
+
+
 
         <hr>
         <h5 class="mt-4 mb-2 content-translate-font mt-3 mb-1">{{ __('school/public-speaking/public-speaking.thewinner') }}
