ccdf2dc4798dbe866dd37280f0f26974efaa7111
Chhaty <chhatyhoeurn@gmail.com>
2024-09-11 16:42:09 +0700

    hi

--- resources/views/web/client/beltei_university/index.blade.php
+++ resources/views/web/client/beltei_university/index.blade.php
@@ -50,8 +50,9 @@
             </a>
         </div>
     </div>
-    
+
     <div class="row gy-2 justify-content-center">
+        <img  data-aos="zoom-out-up"  class="w-75 rounded shadow p-0" src="{{ asset('asset\img\university\app\Doctor_2024-09-11_16-39-16.jpg') }}" alt="">
         <img  data-aos="zoom-out-up"  class="w-75 rounded shadow p-0" src="{{ asset('asset\img\university\app\photo_2024-07-30_19-35-57.jpg') }}" alt="">
         <img  data-aos="zoom-out-up"  class="w-75 rounded shadow p-0" src="{{ asset('asset\img\university\app\photo_2024-07-30_19-35-59.jpg') }}" alt="">
         <img  data-aos="zoom-out-up"  class="w-75 rounded shadow p-0" src="{{ asset('asset\img\university\app\photo_2024-07-05_09-06-49.jpg') }}" alt="">
