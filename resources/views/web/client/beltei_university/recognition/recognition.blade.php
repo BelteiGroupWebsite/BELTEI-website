902c44a6b0ef3ba2c3d05cd8b3a39997c3ab5ac5
sok serey visal <121630407+soksereyvisalBIU@users.noreply.github.com>
2024-10-05 14:48:57 +0700

    Update recognition.blade.php

--- resources/views/web/client/beltei_university/recognition/recognition.blade.php
+++ resources/views/web/client/beltei_university/recognition/recognition.blade.php
@@ -13,14 +13,16 @@
                             (ISO 9001:2015) លើ ប្រព័ន្ធគ្រប់គ្រងគុណភាព (Quality Management System) ពីវិទ្យាស្ថាន
                             ស្តង់ដារកម្ពុជា (ISC) នៃព្រះរាជាណាចក្រកម្ពុជា លេខ No. QMS 006/24 D/DER-ISC</a></td>
                     <td><a href="{{ route('beltei_university.recognition.recognitiondetail') }}?id=1"><img
-                                src="{{ asset('asset/img/school/recognition/ISO9001_2015.png') }}" border="0"></a></td>
+                                style="height: 75px" src="{{ asset('asset/img/university/recognition/ISO9001_2015.png') }}"
+                                border="0"></a></td>
                 </tr>
                 <tr>
                     <td>២</td>
                     <td><a href="{{ route('beltei_university.recognition.recognitiondetail') }}?id=1">វិញ្ញាបនបត្រទទួលស្គាល់គុណភាពអប់រំពេញសិទ្ធិ
                             របស់ ACC លេខ គ.ទ.ក ០១១២០៧២១ FA ចុះថ្ងៃទី២៩ ខែមិថុនា ឆ្នាំ២០២២</a></td>
                     <td><a href="{{ route('beltei_university.recognition.recognitiondetail') }}?id=1"><img
-                                src="{{ asset('asset/img/university/recognition/acc.jpg') }}" border="0"></a></td>
+                                style="height: 75px" src="{{ asset('asset/img/university/recognition/acc.jpg') }}"
+                                border="0"></a></td>
                 </tr>
                 <tr>
                     <td>៣</td>
@@ -28,7 +30,8 @@
                             ការទទួលស្គាល់រចនាសម្ព័ន្ធមហាវិទ្យាល័យ ដេប៉ាតឺម៉ង់ និងមុខជំនាញ ថ្នាក់បរិញ្ញាបត្រ និងបរិញ្ញាបត្ររង
                             លេខ ១៣៩.អយក.ប្រក ថ្ងៃទី២៧ ខែមករា ឆ្នាំ២០២២។</a></td>
                     <td><a href="{{ route('beltei_university.recognition.recognitiondetail') }}?id=2"><img
-                                src="{{ asset('asset/img/school/recognition/01.png') }}" border="0"></a></td>
+                                style="height: 75px" src="{{ asset('asset/img/school/recognition/01.png') }}"
+                                border="0"></a></td>
                 </tr>
                 <tr>
                     <td>៤</td>
@@ -36,28 +39,32 @@
                             ការបើកបណ្តុះបណ្តាលថ្នាក់បរិញ្ញាបត្រជាន់ខ្ពស់ លេខ ១១៨៦.អយក.ប្រក ថ្ងៃទី១៥ ខែធ្នូ ឆ្នាំ២០២១។</a>
                     </td>
                     <td><a href="{{ route('beltei_university.recognition.recognitiondetail') }}?id=3"><img
-                                src="{{ asset('asset/img/school/recognition/01.png') }}" border="0"></a></td>
+                                style="height: 75px" src="{{ asset('asset/img/school/recognition/01.png') }}"
+                                border="0"></a></td>
                 </tr>
                 <tr>
                     <td>៥</td>
                     <td><a href="{{ route('beltei_university.recognition.recognitiondetail') }}?id=4">ប្រកាសស្តីពី
                             ការបើកបណ្តុះបណ្តាលថ្នាក់បណ្ឌិត លេខ ១១៨៧.អយក.ប្រក ថ្ងៃទី១៥ ខែធ្នូ ឆ្នាំ២០២១។</a></td>
                     <td><a href="{{ route('beltei_university.recognition.recognitiondetail') }}?id=4"><img
-                                src="{{ asset('asset/img/school/recognition/01.png') }}" border="0"></a></td>
+                                style="height: 75px" src="{{ asset('asset/img/school/recognition/01.png') }}"
+                                border="0"></a></td>
                 </tr>
                 <tr>
                     <td>៦</td>
                     <td><a href="{{ route('beltei_university.recognition.recognitiondetail') }}?id=5">ប៊ែលធី គ្រុប
                             ទទួលបានពានរង្វាន់ជាអង្គភាពឆ្នើមពីអាស៊ាន</a></td>
                     <td><a href="{{ route('beltei_university.recognition.recognitiondetail') }}?id=5"><img
-                                src="{{ asset('asset/img/school/recognition/layer4.png') }}" border="0"></a></td>
+                                style="height: 75px" src="{{ asset('asset/img/university/recognition/layer4.png') }}"
+                                border="0"></a></td>
                 </tr>
                 <tr>
                     <td>៧</td>
                     <td><a href="{{ route('beltei_university.recognition.recognitiondetail') }}?id=6">វិញ្ញាបនបត្រទទួលស្គាល់ប្រព័ន្ធគ្រប់គ្រងគុណភាពថ្នាក់អន្តរជាតិ
                             (ISO9001:2008)</a></td>
                     <td><a href="{{ route('beltei_university.recognition.recognitiondetail') }}?id=6"><img
-                                src="{{ asset('asset/img/school/recognition/layer9.png') }}" border="0"></a></td>
+                                style="height: 75px" src="{{ asset('asset/img/university/recognition/layer9.png') }}"
+                                border="0"></a></td>
                 </tr>
                 <tr>
                     <td>៨</td>
@@ -65,7 +72,8 @@
                             វិញ្ញាបនបត្របញ្ចាក់ប្រព័ន្ធគ្រប់គ្រងគុណភាពថ្នាក់ជាតិ​ លេខ QMS 006/12 CCD-ISC ថ្ងៃទី១៧ កញ្ញា
                             ២០១២</a></td>
                     <td><a href="{{ route('beltei_university.recognition.recognitiondetail') }}?id=7"><img
-                                src="{{ asset('asset/img/university/recognition/iso.png') }}" border="0"></a></td>
+                                style="height: 75px" src="{{ asset('asset/img/university/recognition/iso.png') }}"
+                                border="0"></a></td>
                 </tr>
                 <tr>
                     <td>៩</td>
@@ -73,7 +81,8 @@
                             វិទ្យាស្ថានប៊ែលធីអន្តរជាតិ ទៅជា សកលវិទ្យាល័យប៊ែលធីអន្តរជាតិ លេខៈ១៣៨.អនក្រ.បក ថ្ងៃទី០៦ ខែកញ្ញា
                             ឆ្នាំ២០១២</a></td>
                     <td><a href="{{ route('beltei_university.recognition.recognitiondetail') }}?id=8"><img
-                                src="{{ asset('asset/img/university/recognition/acc.jpg') }}" border="0"></a></td>
+                                style="height: 75px" src="{{ asset('asset/img/university/recognition/acc.jpg') }}"
+                                border="0"></a></td>
                 </tr>
                 <tr>
                     <td>១០</td>
@@ -81,7 +90,8 @@
                             របស់ ACC លេខៈ គ.ទ.ក ០២៤៦១១១២ ចុះថ្ងៃទី១៩ ខែកញ្ញា ឆ្នាំ២០១១ ចុះហត្ថលេខាដោយ ឯកឧត្តម សាម មន្តិយ៉ា
                         </a></td>
                     <td><a href="{{ route('beltei_university.recognition.recognitiondetail') }}?id=9"><img
-                                src="{{ asset('asset/img/school/recognition/01.png') }}" border="0"></a></td>
+                                style="height: 75px" src="{{ asset('asset/img/school/recognition/01.png') }}"
+                                border="0"></a></td>
                 </tr>
 
                 <tr>
@@ -90,7 +100,8 @@
                             របស់ ACC លេខៈ គ.ទ.ក ០២៤៦០៨៣២ ចុះថ្ងៃទី២២ ខែតុលា ឆ្នាំ២០០៨ ចុះហត្ថលេខាដោយ ឯកឧត្តម សុខ អាន </a>
                     </td>
                     <td><a href="{{ route('beltei_university.recognition.recognitiondetail') }}?id=10"><img
-                                src="{{ asset('asset/img/school/recognition/01.png') }}" border="0"></a></td>
+                                style="height: 75px" src="{{ asset('asset/img/school/recognition/01.png') }}"
+                                border="0"></a></td>
                 </tr>
                 <tr>
                     <td>១២</td>
@@ -98,7 +109,8 @@
                             លេខៈ១៤២៦.អយក.ប្រក ថ្ងៃទី៣១ ខែសីហា​ឆ្នាំ២០០៦ ការ ទទួល ស្គាល់ ដេប៉ា ដឺម៉ងថ្នាក់ឆ្នាំសិក្សាមូលដ្ឋាន
                         </a></td>
                     <td><a href="{{ route('beltei_university.recognition.recognitiondetail') }}?id=11"><img
-                                src="{{ asset('asset/img/school/recognition/01.png') }}" border="0"></a></td>
+                                style="height: 75px" src="{{ asset('asset/img/school/recognition/01.png') }}"
+                                border="0"></a></td>
                 </tr>
                 <tr>
                     <td>១៣</td>
@@ -106,7 +118,8 @@
                             វិទ្យាស្ថានអាស៊ីប៉ាស៊ីភិច ទៅជា វិទ្យាស្ថានប៊ែលធីអន្តរជាតិ លេខៈ៩៧ ថ្ងៃទី០៤ ខែសីហា ឆ្នាំ២០០៥ </a>
                     </td>
                     <td><a href="{{ route('beltei_university.recognition.recognitiondetail') }}?id=12"><img
-                                src="{{ asset('asset/img/school/recognition/01.png') }}" border="0"></a></td>
+                                style="height: 75px" src="{{ asset('asset/img/school/recognition/01.png') }}"
+                                border="0"></a></td>
                 </tr>
                 <tr>
 
