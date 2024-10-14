d5932c30bc8e61584550d35d766551b0eadc052a
sok serey visal <121630407+soksereyvisalBIU@users.noreply.github.com>
2024-10-14 14:30:49 +0700

    Update certificate-search.blade.php

--- resources/views/livewire/university/certificate-search.blade.php
+++ resources/views/livewire/university/certificate-search.blade.php
@@ -1,18 +1,20 @@
 <div>
 
-    
+
     <div class="title-translate-font mb-4">
         @php
             use Carbon\Carbon;
-            if(app()->getLocale() == "kh"){
+            if (app()->getLocale() == 'kh') {
                 $degree = $batchCert->degree->degree_kh;
-            }else{
+            } else {
                 $degree = $batchCert->degree->degree_eng;
             }
-            
+
         @endphp
 
-        <h4 class="Blue-color">{{ $degree }} {{ __('beltei_university/certificate/certificate.batch') }} {{ $batchCert->batch }} {{ __('beltei_university/certificate/certificate.year') }} {{ $batchCert->academic_year . "-" .$batchCert->academic_year + 1 }}</h4>
+        <h4 class="Blue-color">{{ $degree }} {{ __('beltei_university/certificate/certificate.batch') }}
+            {{ $batchCert->batch }} {{ __('beltei_university/certificate/certificate.year') }}
+            {{ $batchCert->academic_year . '-' . $batchCert->academic_year + 1 }}</h4>
         <h5 class="mt-3">{{ __('beltei_university/certificate/certificate.find') }}</h5>
     </div>
 
@@ -28,8 +30,9 @@
 
 
     <div class="w-100 overflow-auto">
-        <table class="table table-responsive content-translate-font table-hover table-bordered w-100 f14" style="vertical-align: middle">
-    
+        <table class="table table-responsive content-translate-font table-hover table-bordered w-100 f14"
+            style="vertical-align: middle">
+
             <thead class="table-info font-bold">
                 <tr>
                     <td>N<sup>o</sup></td>
@@ -40,73 +43,81 @@
                     <td>{{ __('beltei_university/certificate/certificate.profile') }}</td>
                     <td>{{ __('beltei_university/certificate/certificate.certificate') }}</td>
                 </tr>
-                
+
             </thead>
-        
+
             <tbody class="f14">
-    
+
                 {{-- {{ dd($certificates->groupBy('major_id')) }} --}}
-        
+
                 @foreach ($certificates->groupBy('major_id') as $certificatesGroup)
                     @php
                         $facultyColor = $certificatesGroup->first()->major->faculty->color;
-                        
-                        if(app()->getLocale() == "kh"){
+
+                        if (app()->getLocale() == 'kh') {
                             $faculty = $certificatesGroup->first()->major->faculty->FacultyKhmer;
                             $major = $certificatesGroup->first()->major->MajorKhmer;
-                        }else{
+                        } else {
                             $major = $certificatesGroup->first()->major->MajorEnglish;
                             $faculty = $certificatesGroup->first()->major->faculty->FacultyEnglish;
                         }
-                        
+
                     @endphp
-                <tr>
-                    <td colspan="10" class="text-uppercase text-white title-translate-font" style="background-color: {{ $facultyColor }}; border-color: {{ $facultyColor }} !important;">
-                        <h6>{{ __('beltei_university/certificate/certificate.faculty') }} {{ $faculty }}</h6>
-                        <p>{{ __('beltei_university/certificate/certificate.major') }} {{ $major }}</p>
-                    </td>
-                </tr>
-                @foreach ($certificatesGroup as $certificate)
-                    @php
-                        $certificateNo = $certificate->certi_no;
-                        $profileImagePath = asset("storage/upload/certificate/university/{$degreeId}/{$batchId}/profile/{$certificateNo}.jpg");
-                        $certificateImagePath = asset("storage/upload/certificate/university/{$degreeId}/{$batchId}/beltei/{$certificateNo}.jpg");
-    
-                        if(app()->getLocale() == "kh"){
-                            $name = $certificate->khmer_name;
-                        }else{
-                            $name = $certificate->latin_name;
-                        }
-                        
-                    @endphp
-                    <tr style="border-color: {{ $facultyColor }} !important;">
-                        <td>{{ $certificateNo }}</td>
-                        <td>{{ $name }}</td>
-                        <td>{{ $certificate->gender }}</td>
-                        <td>{{ Carbon::parse($certificate->dob)->format('d/M/Y') }}</td>
-                        <td>
-                            <a href="{{ $profileImagePath }}">
-                                <img loading="lazy" style="max-width: 80px" src="{{ $profileImagePath }}" alt="profile">
-                            </a>
-                        </td>
-                        <td>
-                            <a href="{{ $certificateImagePath }}">
-                                <img loading="lazy" style="max-width: 80px" src="{{ $certificateImagePath }}" alt="certificate">
-                            </a>
+                    <tr>
+                        <td colspan="10" class="text-uppercase text-white title-translate-font"
+                            style="background-color: {{ $facultyColor }}; border-color: {{ $facultyColor }} !important;">
+                            <h6>{{ __('beltei_university/certificate/certificate.faculty') }} {{ $faculty }}</h6>
+                            <p>{{ __('beltei_university/certificate/certificate.major') }} {{ $major }}</p>
                         </td>
                     </tr>
+                    @foreach ($certificatesGroup as $certificate)
+                        @php
+                            $certificateNo = $certificate->certi_no;
+                            $profileNo = $certificate->profile_no;
+                            $profileImagePath = asset(
+                                "storage/upload/certificate/university/{$degreeId}/{$batchId}/profile/{$profileNo}.jpg",
+                            );
+                            $certificateImagePath = asset(
+                                "storage/upload/certificate/university/{$degreeId}/{$batchId}/beltei/{$certificateNo}.jpg",
+                            );
+
+                            if (app()->getLocale() == 'kh') {
+                                $name = $certificate->khmer_name;
+                            } else {
+                                $name = $certificate->latin_name;
+                            }
+
+                        @endphp
+                        <tr style="border-color: {{ $facultyColor }} !important;">
+                            <td>{{ $certificateNo }}</td>
+                            <td>{{ $name }}</td>
+                            <td>{{ $certificate->gender }}</td>
+                            <td>{{ Carbon::parse($certificate->dob)->format('d/M/Y') }}</td>
+                            <td>
+                                <a href="{{ $profileImagePath }}">
+                                    <img loading="lazy" style="max-width: 80px" src="{{ $profileImagePath }}"
+                                        alt="profile">
+                                </a>
+                            </td>
+                            <td>
+                                <a href="{{ $certificateImagePath }}">
+                                    <img loading="lazy" style="max-width: 80px" src="{{ $certificateImagePath }}"
+                                        alt="certificate">
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
                 @endforeach
-            @endforeach
-            
-            
-                
+
+
+
             </tbody>
-            
+
         </table>
-    
+
         {{ $certificates->links('vendor.livewire.bootstrap') }}
     </div>
-    
-    
-    
+
+
+
 </div>
