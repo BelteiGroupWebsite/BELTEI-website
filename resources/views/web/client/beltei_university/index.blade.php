f648a9e5087112f701caa2bf5aca1d10f2b0a10d
sok serey visal <121630407+soksereyvisalBIU@users.noreply.github.com>
2024-10-28 10:39:48 +0700

    update

--- resources/views/web/client/beltei_university/index.blade.php
+++ resources/views/web/client/beltei_university/index.blade.php
@@ -30,41 +30,58 @@
 @extends('web.client.beltei_university.layout.app')
 
 @section('content')
-<div>
-
     <div>
+
         <div>
-            <img data-aos="zoom-out-up" class="" style="" src="{{ asset('asset\img\university\app\beltei_the_future_of_global_leaders.gif') }}">
+            <div>
+                <img data-aos="zoom-out-up" class="" style=""
+                    src="{{ asset('asset/img/university/app/beltei_the_future_of_global_leaders.gif') }}">
+            </div>
+            <div>
+                <a href="{{ route('beltei_university.bicc.bicc') }}">
+                    <img data-aos="zoom-out-up" class="" style="max-width: 200px"
+                        src="{{ asset('asset/img/university/app/BICC.jpg') }}">
+                </a>
+            </div>
+            <div>
+                <a href="https://www.facebook.com/belteiuniversity">
+                    <img data-aos="zoom-out-up" class="" style="max-width: 50%"
+                        src="{{ asset('asset/img/university/app/photo_2024-07-05_09-27-01.jpg') }}">
+                </a>
+                <a href="https://www.youtube.com/channel/UCV8pvuK4Wu0l-K1o38h5iag">
+                    <img data-aos="zoom-out-up" class="" style="max-width: 50%"
+                        src="{{ asset('asset/img/university/app/youtube.jpg') }}">
+                </a>
+            </div>
         </div>
-        <div>
-            <a href="{{ route('beltei_university.bicc.bicc') }}">
-                <img data-aos="zoom-out-up" class="" style="max-width: 200px" src="{{ asset('asset\img\university\app\BICC.jpg') }}">
-            </a>
+
+        <div class="row gy-2 justify-content-center">
+            <img data-aos="zoom-out-up" class="w-75 rounded shadow p-0"
+                src="{{ asset('asset/img/university/app/F.ChineseIntakedone.jpg') }}" alt="">
+            <img data-aos="zoom-out-up" class="w-75 rounded shadow p-0"
+                src="{{ asset('asset/img/university/app/F.ChineseIntake.jpg') }}" alt="">
+            <img data-aos="zoom-out-up" class="w-75 rounded shadow p-0"
+                src="{{ asset('asset/img/university/app/new_term_2024_2025.jpg') }}" alt="">
+            <img data-aos="zoom-out-up" class="w-75 rounded shadow p-0"
+                src="{{ asset('asset/img/university/app/New_term_3.jpg') }}" alt="">
+            <img data-aos="zoom-out-up" class="w-75 rounded shadow p-0"
+                src="{{ asset('asset/img/university/app/New_term_4.jpg') }}" alt="">
+            <img data-aos="zoom-out-up" class="w-75 rounded shadow p-0"
+                src="{{ asset('asset/img/university/app/New_term_5.jpg') }}" alt="">
+            <img data-aos="zoom-out-up" class="w-75 rounded shadow p-0"
+                src="{{ asset('asset/img/university/app/New_term_6.jpg') }}" alt="">
+            <img data-aos="zoom-out-up" class="w-75 rounded shadow p-0" src="{{ asset('asset/img/university/app/3.jpg') }}"
+                alt="">
+            <img data-aos="zoom-out-up" class="w-75 rounded shadow p-0" src="{{ asset('asset/img/university/app/4.jpg') }}"
+                alt="">
+            <img data-aos="zoom-out-up" class="w-75 rounded shadow p-0" src="{{ asset('asset/img/university/app/5.jpg') }}"
+                alt="">
+            <img data-aos="zoom-out-up" class="w-75 rounded shadow p-0"
+                src="{{ asset('asset/img/university/app/Doctor_2024-09-11_16-39-16.jpg') }}" alt="">
+
         </div>
-        <div>
-            <a href="https://www.facebook.com/belteiuniversity">
-                <img data-aos="zoom-out-up" class="" style="max-width: 50%" src="{{ asset('asset\img\university\app\photo_2024-07-05_09-27-01.jpg') }}">
-            </a>
-            <a href="https://www.youtube.com/channel/UCV8pvuK4Wu0l-K1o38h5iag">
-                <img data-aos="zoom-out-up" class="" style="max-width: 50%" src="{{ asset('asset\img\university\app\youtube.jpg') }}">
-            </a>
-        </div>
+
     </div>
-
-    <div class="row gy-2 justify-content-center">
-        <img  data-aos="zoom-out-up"  class="w-75 rounded shadow p-0" src="{{ asset('asset\img\university\app\new_term_2024_2025.jpg') }}" alt="">
-        <img  data-aos="zoom-out-up"  class="w-75 rounded shadow p-0" src="{{ asset('asset\img\university\app\New_term_3.jpg') }}" alt="">
-        <img  data-aos="zoom-out-up"  class="w-75 rounded shadow p-0" src="{{ asset('asset\img\university\app\New_term_4.jpg') }}" alt="">
-        <img  data-aos="zoom-out-up"  class="w-75 rounded shadow p-0" src="{{ asset('asset\img\university\app\New_term_5.jpg') }}" alt="">
-        <img  data-aos="zoom-out-up"  class="w-75 rounded shadow p-0" src="{{ asset('asset\img\university\app\New_term_6.jpg') }}" alt="">
-        <img  data-aos="zoom-out-up"  class="w-75 rounded shadow p-0" src="{{ asset('asset\img\university\app\3.jpg') }}" alt="">
-        <img  data-aos="zoom-out-up"  class="w-75 rounded shadow p-0" src="{{ asset('asset\img\university\app\4.jpg') }}" alt="">
-        <img  data-aos="zoom-out-up"  class="w-75 rounded shadow p-0" src="{{ asset('asset\img\university\app\5.jpg') }}" alt="">
-        <img  data-aos="zoom-out-up"  class="w-75 rounded shadow p-0" src="{{ asset('asset\img\university\app\Doctor_2024-09-11_16-39-16.jpg') }}" alt="">
-
-    </div>
-
-</div>
 
 
     @include('web.client.beltei_university.layout.share.news')
