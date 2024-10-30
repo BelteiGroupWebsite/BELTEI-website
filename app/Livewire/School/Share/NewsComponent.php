9757c31ec818c5e5e3232e1e081de40a13d9e86c
sok serey visal <121630407+soksereyvisalBIU@users.noreply.github.com>
2024-10-30 09:55:07 +0700

    update school news pagination

---
+++ app/Livewire/School/Share/NewsComponent.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Livewire\School\Share;
+
+use App\Models\University\News;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class NewsComponent extends Component
+{
+    use WithPagination;
+    public function render()
+    {
+        $bis_news = News::where('category', 3)->orderBy('date', 'desc')->paginate(10);
+        return view('livewire.school.share.news-component' , compact('bis_news'));
+    }
+}
