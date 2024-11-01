57ebfb830d1e5c2a1b916ff1838d40a13414c091
sok serey visal <121630407+soksereyvisalBIU@users.noreply.github.com>
2024-11-01 09:02:11 +0700

    Update NewsComponent.php

--- app/Livewire/School/Share/NewsComponent.php
+++ app/Livewire/School/Share/NewsComponent.php
@@ -11,7 +11,7 @@ class NewsComponent extends Component
     use WithPagination;
     public function render()
     {
-        $bis_news = News::where('category', 3)->orderBy('date', 'desc')->paginate(10);
-        return view('livewire.school.share.news-component' , compact('bis_news'));
+        $bis_news = News::where('category', 2)->orderBy('date', 'desc')->paginate(10);
+        return view('livewire.school.share.news-component', compact('bis_news'));
     }
 }
