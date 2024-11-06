eaa823467e632ca618cc51d2c6b9068fb7f9c9b5
sok serey visal <121630407+soksereyvisalBIU@users.noreply.github.com>
2024-11-06 11:07:23 +0700

    Update whyBeltei.blade.php

--- resources/views/web/client/beltei_university/about/whyBeltei.blade.php
+++ resources/views/web/client/beltei_university/about/whyBeltei.blade.php
@@ -10,16 +10,16 @@
             <img class="w-75 text-center my-3" src="{{ asset('asset/img/university/about/whybeltei.jpg') }}" alt="">
             <div class="row mt-2">
                 <div class="col-6 p-2">
-                    <img class="text-center" src="{{ asset('asset/img/university/about/1.png') }}" alt="">
+                    <img class="w-100 text-center" src="{{ asset('asset/img/university/about/1.png') }}" alt="">
                 </div>
                 <div class="col-6 p-2">
-                    <img class="text-center" src="{{ asset('asset/img/university/about/2.png') }}" alt="">
+                    <img class="w-100 text-center" src="{{ asset('asset/img/university/about/2.png') }}" alt="">
                 </div>
                 <div class="col-6 p-2">
-                    <img class="text-center" src="{{ asset('asset/img/university/about/3.png') }}" alt="">
+                    <img class="w-100 text-center" src="{{ asset('asset/img/university/about/3.png') }}" alt="">
                 </div>
                 <div class="col-6 p-2">
-                    <img class="text-center" src="{{ asset('asset/img/university/about/4.png') }}" alt="">
+                    <img class="w-100 text-center" src="{{ asset('asset/img/university/about/4.png') }}" alt="">
                 </div>
             </div>
         </div>
