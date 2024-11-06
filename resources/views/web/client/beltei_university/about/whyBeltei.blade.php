e71fa876fbea2bec6c2bd5bc67e89b192b856dd5
sok serey visal <121630407+soksereyvisalBIU@users.noreply.github.com>
2024-11-06 11:01:45 +0700

    Update whyBeltei.blade.php

--- resources/views/web/client/beltei_university/about/whyBeltei.blade.php
+++ resources/views/web/client/beltei_university/about/whyBeltei.blade.php
@@ -2,16 +2,19 @@
 
 
 @section('content')
-<div class="p-lg-3">
+    <div class="p-lg-3">
 
         <h4 class="title-translate-font my-3 underline Blue-color">{{ __('beltei_university/about/whyBeltei.header') }}</h4>
 
         <div>
             <img class="w-75 text-center my-3" src="{{ asset('asset/img/university/about/whybeltei.jpg') }}" alt="">
-            <img class="w-75 text-center my-3" src="{{ asset('asset/img/university/about/1.png') }}" alt="">
-            <img class="w-75 text-center my-3" src="{{ asset('asset/img/university/about/2.png') }}" alt="">
-            <img class="w-75 text-center my-3" src="{{ asset('asset/img/university/about/3.png') }}" alt="">
-            <img class="w-75 text-center my-3" src="{{ asset('asset/img/university/about/4.png') }}" alt="">
+            <div class="row">
+
+                <img class="col-6 text-center my-3" src="{{ asset('asset/img/university/about/1.png') }}" alt="">
+                <img class="col-6 text-center my-3" src="{{ asset('asset/img/university/about/2.png') }}" alt="">
+                <img class="col-6 text-center my-3" src="{{ asset('asset/img/university/about/3.png') }}" alt="">
+                <img class="col-6 text-center my-3" src="{{ asset('asset/img/university/about/4.png') }}" alt="">
+            </div>
         </div>
 
         <div class="content-translate-font text-justify">
