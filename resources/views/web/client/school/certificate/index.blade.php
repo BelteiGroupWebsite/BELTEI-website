a6eb560dccb22c40d11a00fdc33f593828075299
sok serey visal <121630407+soksereyvisalBIU@users.noreply.github.com>
2024-11-28 15:31:29 +0700

    Update index.blade.php

--- resources/views/web/client/school/certificate/index.blade.php
+++ resources/views/web/client/school/certificate/index.blade.php
@@ -37,7 +37,7 @@
                 </tr> --}}
                 <tr class="" style="background-color: lightblue">
                     {{-- {{ $program . ' ' . $arr }} --}}
-                    @if (is_array($arr) && !in_array($program, $arr))
+                    @if (is_array($arr) && !in_array($grades->id, $arr))
                         <td rowspan="2">{{ __('school/certificate/index.year') }}</td>
                     @endif
 
