54ee746def91446294ae75bf0b57f238a0125105
sok serey visal <121630407+soksereyvisalBIU@users.noreply.github.com>
2024-12-26 09:05:39 +0700

    update translate

---
+++ resources/lang/en/school/extracurriculum/generalknowledge.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'header-1' => "1. Understanding of Drug and E-cigarette (Vape) (Grade 7 & 8 - Level 7 & 8)",
+    'header-2' => "2. Traffic Law (Grade 7 & 8 - Level 7 & 8)",
+    'header-3' => "3. Concept of Buddhism (Grad 9 - Level 9)",
+    'header-4' => "4. Food Safety (Grade 10-Level 10)",
+    'header-5' => "5. Cambodian Youth The digital age (Grade 10- Level 10)",
+    'header-6' => "6. Role of Youths in society (Grade 11 - Level 11)",
+    'header-7' => "7. Awareness of the Security Regime (Grade 12-Level 12)",
+
+    'detail-1' => "Students learn about hazards of drug and E-cigarette (Vape) abuse, how to protect themselves from drug use, never try nor use any drug, training them on how to educate others not to self-indulge into drug abuse and to understand the value of relationships in their families, schools, and society.",
+    'detail-2' => "Students learn about traffic laws, traffic signs, traffic situations, causes of road accidents, and traffic jams in Cambodia, especially they are aware of safety tips and vehicle control & check before or while driving in order to avert any road eventual accidents.",
+    'detail-3' => "Students are aware of Dharma; promoting Buddhism which is the state religion, imbuing the doctrines of Buddhist regarding good deeds, bad deeds, sins, particularly educating students about morality & ethics and virtue, instructing students how to abstain drug abuse, domestic violence, pedophilic acts, rapes, road accidents, and bullying.",
+    'detail-4' => "Students learn about food hygiene, which has good nutrition and safe. learn how to take care of health and learn how to keep food wisely and kitchenware hygiene, and how to avoid the diseases which is caused by hazardous food.",
+    'detail-5' => "Students learn about the characteristics of Cambodian youth in the digital era. It Require to have extensive knowledge, skills, intelligence, good morals with great attitudes for industrial revolution 4.0, to become a new, modern, and global youth to participate in the defense of our homeland and towards national prosperity.",
+    'detail-6' => "Students learn about the benefits and features of the National Social Security Fund (NSSF) on Occupational Risk Scheme, Health Care Scheme (HCS), and Pension Scheme, so that students are ready to enter the workforce after graduation.",
+    'detail-7' => "Students understand the principles and benefits of the National Social Security Fund (NSSF) on the social security system: 1- Occupational Risk Scheme (a. Occupational accidents in the workplace or during travel; b. Occupational diseases such as Toxic Occupational Chef) 2- Health Care Scheme (Social Health Insurance to provide preventive and medical care services, as well as daily allowances during sick leave and maternity leave) and 3- Pension (a. Receive health insurance service upon retirement; b. Receive a monthly pension (twice a month); c. Dependents can receive a monthly allowance upon retirement or death; d. Receive 2 million Riels for cremation at the time of death of the retiree).",
+
+];
--- resources/views/web/client/school/extracurriculum/generalknowledge.blade.php
+++ resources/views/web/client/school/extracurriculum/generalknowledge.blade.php
@@ -7,14 +7,10 @@
 
             <div class="py-2">
                 <h5>
-                    1. Understanding of Drug and E-cigarette (Vape) (Grade 7 & 8 - Level 7 & 8)
+                    {!! __('school/extracurriculum/generalknowledge.header-1') !!}
                 </h5>
                 <p class="indent-1rem pt-1">
-                    Students learn about hazards of drug and E-cigarette (Vape) abuse, how to protect themselves from drug
-                    use,
-                    never try nor use any drug, training them on how to educate others not to self-indulge into drug abuse
-                    and
-                    to understand the value of relationships in their families, schools, and society.
+                    {!! __('school/extracurriculum/generalknowledge.detail-1') !!}
                 </p>
                 <div class="row ">
                     @for ($i = 1; $i <= 10; $i++)
@@ -26,12 +22,10 @@
             </div>
             <div class="py-2">
                 <h5>
-                    2. Traffic Law (Grade 7 & 8 - Level 7 & 8)
+                    {!! __('school/extracurriculum/generalknowledge.header-2') !!}
                 </h5>
                 <p class="indent-1rem pt-1">
-                    Students learn about traffic laws, traffic signs, traffic situations, causes of road accidents, and
-                    traffic jams in Cambodia, especially they are aware of safety tips and vehicle control & check before or
-                    while driving in order to avert any road eventual accidents.
+                    {!! __('school/extracurriculum/generalknowledge.detail-2') !!}
 
                 </p>
                 <div class="row ">
@@ -44,13 +38,10 @@
             </div>
             <div class="py-2">
                 <h5>
-                    3. Concept of Buddhism (Grad 9 - Level 9)
+                    {!! __('school/extracurriculum/generalknowledge.header-3') !!}
                 </h5>
                 <p class="indent-1rem pt-1">
-                    Students are aware of Dharma; promoting Buddhism which is the state religion, imbuing the doctrines of
-                    Buddhist regarding good deeds, bad deeds, sins, particularly educating students about morality & ethics
-                    and virtue, instructing students how to abstain drug abuse, domestic violence, pedophilic acts, rapes,
-                    road accidents, and bullying.
+                    {!! __('school/extracurriculum/generalknowledge.detail-3') !!}
                 </p>
                 <div class="row ">
                     @for ($i = 1; $i <= 10; $i++)
@@ -62,12 +53,10 @@
             </div>
             <div class="py-2">
                 <h5>
-                    4. Food Safety (Grade 10-Level 10)
+                    {!! __('school/extracurriculum/generalknowledge.header-4') !!}
                 </h5>
                 <p class="indent-1rem pt-1">
-                    Students learn about food hygiene, which has good nutrition and safe. learn how to take care of health
-                    and learn how to keep food wisely and kitchenware hygiene, and how to avoid the diseases which is caused
-                    by hazardous food.
+                    {!! __('school/extracurriculum/generalknowledge.detail-4') !!}
                 </p>
                 <div class="row ">
                     @for ($i = 1; $i <= 10; $i++)
@@ -79,13 +68,10 @@
             </div>
             <div class="py-2">
                 <h5>
-                    5. Cambodian Youth The digital age (Grade 10- Level 10)
+                    {!! __('school/extracurriculum/generalknowledge.header-5') !!}
                 </h5>
                 <p class="indent-1rem pt-1">
-                    Students learn about the characteristics of Cambodian youth in the digital era. It Require to have
-                    extensive knowledge, skills, intelligence, good morals with great attitudes for industrial revolution
-                    4.0, to become a new, modern, and global youth to participate in the defense of our homeland and towards
-                    national prosperity.
+                    {!! __('school/extracurriculum/generalknowledge.detail-5') !!}
 
                 </p>
                 <div class="row ">
@@ -98,12 +84,10 @@
             </div>
             <div class="py-2">
                 <h5>
-                    6. Role of Youths in society (Grade 11 - Level 11)
+                    {!! __('school/extracurriculum/generalknowledge.header-6') !!}
                 </h5>
                 <p class="indent-1rem pt-1">
-                    Students learn about the benefits and features of the National Social Security Fund (NSSF) on
-                    Occupational Risk Scheme, Health Care Scheme (HCS), and Pension Scheme, so that students are ready to
-                    enter the workforce after graduation.
+                    {!! __('school/extracurriculum/generalknowledge.detail-6') !!}
 
 
                 </p>
@@ -117,16 +101,10 @@
             </div>
             <div class="py-2">
                 <h5>
-                    7. Awareness of the Security Regime (Grade 12-Level 12)
+                    {!! __('school/extracurriculum/generalknowledge.header-7') !!}
                 </h5>
                 <p class="indent-1rem pt-1">
-                    Students understand the principles and benefits of the National Social Security Fund (NSSF) on the
-                    social security system: 1- Occupational Risk Scheme (a. Occupational accidents in the workplace or
-                    during travel; b. Occupational diseases such as Toxic Occupational Chef) 2- Health Care Scheme (Social
-                    Health Insurance to provide preventive and medical care services, as well as daily allowances during
-                    sick leave and maternity leave) and 3- Pension (a. Receive health insurance service upon retirement; b.
-                    Receive a monthly pension (twice a month); c. Dependents can receive a monthly allowance upon retirement
-                    or death; d. Receive 2 million Riels for cremation at the time of death of the retiree).
+                    {!! __('school/extracurriculum/generalknowledge.detail-7') !!}
 
 
 
