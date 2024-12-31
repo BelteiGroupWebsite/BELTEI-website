1c9dd300efb10cbe327c4470345b70177c24d95c
sok serey visal <121630407+soksereyvisalBIU@users.noreply.github.com>
2024-12-31 08:34:58 +0700

    Update news.blade.php

--- resources/views/web/client/charity/layout/share/news.blade.php
+++ resources/views/web/client/charity/layout/share/news.blade.php
@@ -4,90 +4,104 @@
         <h6 class="Muol-Light p-3 my-2 bg-success text-white">ព្រឹត្តិការណ៍សំខាន់ៗប្រចាំថ្ងៃ</h6>
     </div>
     <hr>
+
     {{-- Hot News  --}}
-    <a href="{{ route('school.news.') }}?id=1" class="card card-body d-flex">
+    <a href="{{ route('beltei_university.news.detail', $charity_news->first()->id) }}" class="card card-body d-flex">
         {{-- <a href="{{ route('school.news.bacll-batch18') }}" class="card card-body d-flex"> --}}
         <div class="overflow-hidden">
-            <img class="w-100" src="{{ asset('asset/img/school/dailyNews/1/1.jpg') }}" alt="">
+            @if ($charity_news->first()->image)
+                @foreach (array_slice(explode(',', $charity_news->first()->image), 0, 1) as $newsImg)
+                    <img class="rounded shadow-sm width-100 w-100" loading="lazy"
+                        src="{{ asset('uploaded/university/news/images/' . $charity_news->first()->id . '/' . $newsImg) }}"
+                        alt="News Image">
+                @endforeach
+            @endif
+            {{-- <img class="w-100" src="{{ asset('asset/img/school/dailyNews/1/1.jpg') }}" alt=""> --}}
         </div>
         <div class="p-3">
             <h6 class="Muol-Light line-height-15">
-                សា​លា​ ប៊ែល​ធី​ អន្តរ​ជាតិ​ បាន​ប្រារព្ធ​ពិធី​ប្រគល់
-                ​សញ្ញា​បត្រ​មធ្យម​សិក្សា​ទុតិយ​ភូមិ​ប៊ែល​ធី​ជូន​ដល់​សិស្ស​
-                ​ជ័យ​លា​ភី​ថ្នាក់​ទី​១២​ ជំ​នាន់​ទី​១៨​ ចំ​នួន​ ១ ០៧៣​នាក់​ (​ក្រុម​ទី​១​)
+                @foreach ($charity_news->first()->newsDetail as $item)
+                    @if (app()->getLocale() == 'kh')
+                        @if ($item->language_id == 1)
+                            <div onclick="toggleDescription(this)" class="cursor-pointer "
+                                data-fulltext="{{ $item->header }}">
+                                {{ Str::limit($item->header, 200) }}
+                            </div>
+                        @endif
+                    @else
+                        @if ($item->language_id == 2)
+                            <div onclick="toggleDescription(this)" class="cursor-pointer "
+                                data-fulltext="{{ $item->header }}">
+                                {{ Str::limit($item->header, 200) }}
+                            </div>
+                        @endif
+                    @endif
+                @endforeach
             </h6>
-            <p class="Battambang-Regular f14 line-height-15">​
-                ដែល​បាន​ប្រឡង​ជាប់​សញ្ញា​បត្រ​មធ្យម​សិក្សា​ទុតិ​យ​ភូមិ​ (​បាក់​ឌុប​)​
-                មក​ពី​សា​ខា​ទី​១​ ដល់​សា​ខា​ទី​១០​ ក្រោម​អធិប​តី​ភាព​ដ៏​ខ្ពង់​ខ្ពស់​របស់​
-                ឯក​ឧត្តម​បណ្ឌិត​ លី​ ឆេង​ តំណាង​រាស្រ្ត​មណ្ឌល​រាជ​ធានី​ភ្នំពេញ​ ស្ថាប​និក​
-                និង​ជា​អគ្គ​នា​យក​ ប៊ែល​ធី​ គ្រុប​ ព្រម​ទាំង​លោក​ជំ​ទាវ​។​
-                ពិ​ធី​នេះ​បាន​ប្រព្រឹត្ត​ទៅ​ នា​ព្រឹក​ថ្ងៃ​សៅរ៍​ ១​រោច​ ខែ​បុស្ស​ ឆ្នាំ​ថោះ​
-                បញ្ចស័ក​ ព.ស.​២៥៦៧​ ត្រូវ​នឹង​ថ្ងៃ​ទី​២៧​ ខែ​មករា​ ឆ្នាំ​២០២៤​ នៅ​សាល​សន្និ​សីទ​
-                នៃ​សាកល​វិទ្យា​ល័យ​ ប៊ែលធី​ អន្តរ​ជាតិ​ទី​តាំង​ទី​២​ (​ស្ពាន​អាកាស​ចោម​ចៅ​)​។
-            </p>
+            {{-- <p class="Battambang-Regular f14 line-height-15">​
+                    @foreach ($charity_news->first()->newsDetail as $item)
+                        @if ($item->language_id == 1)
+                            <div onclick="toggleDescription(this)" class="cursor-pointer "
+                                data-fulltext="{{ $item->description }}">
+                                {!! Str::limit($item->description) !!}
+                            </div>
+                        @endif
+                    @endforeach
+                </p> --}}
         </div>
     </a>
 
-    {{-- Other News  --}}
+
     <div class="row p-0 m-0">
 
+        @foreach ($charity_news as $news)
+            {{-- <a href="{{ route('school.news.') }}?id=2" class="col-6 d-flex align-items-center border"> --}}
+            <a href="{{ route('beltei_university.news.detail', $news->id) }}"
+                class="col-6 row align-items-center border m-0">
+                <div class=" col-6 overflow-hidden">
 
-        <a href="{{ route('school.news.') }}?id=2" class="col-6 d-flex align-items-center border">
-            <div class="max-width-150 overflow-hidden">
-                <img class="w-100" src="{{ asset('asset/img/school/dailyNews/2/2.jpg') }}" alt="">
-            </div>
-            <div class="p-3 ">
-                <h6 class="Muol-Light f12 line-height-15">
-                    ​ពិធី​ប្រគល់
-                    ​សញ្ញា​បត្រ​មធ្យម​សិក្សា​បឋម​ភូមិ​ប៊ែលធី
-                </h6>
-                <p class="Battambang-Regular f12 line-height-15">​
-                    ​ជូន​ដល់​សិស្ស​ជ័យ​លាភី​ថ្នាក់​ទី​៩​ ជំ​នាន់​ទី​១៨​ ចំ​នួន​ ១ ០៤១​នាក់​ (​ក្រុម​ទី​៣​)​
-                </p>
-            </div>
-        </a>
+                    @if ($news->image)
+                        @foreach (array_slice(explode(',', $news->image), 0, 1) as $newsImg)
+                            <img class="rounded shadow-sm w-100" loading="lazy"
+                                src="{{ asset('uploaded/university/news/images/' . $news->id . '/' . $newsImg) }}"
+                                alt="News Image">
+                        @endforeach
+                    @endif
 
-        <a href="{{ route('school.news.') }}?id=3" class="col-6 d-flex align-items-center border">
-            <div class="max-width-150 overflow-hidden">
-                <img class="w-100" src="{{ asset('asset/img/school/dailyNews/3/8.jpg') }}" alt="">
-            </div>
-            <div class="p-3 ">
-                <h6 class="Muol-Light f12 line-height-15">
-                    ប្រារព្ធ​ពិធី​ប្រគល់
-                    ​សញ្ញា​បត្រ​មធ្យម​សិក្សា​បឋម​ភូមិ​ប៊ែលធី</h6>
-                <p class="Battambang-Regular f12 line-height-15">​
-                    សា​លា​ ប៊ែល​ធី​ អន្តរ​ជាតិបាន​ប្រារព្ធ​ពិធី​ប្រគល់​សញ្ញា​បត្រ​មធ្យម​សិក្សា​បឋម​ភូមិ​ប៊ែល​ធី​​
-                </p>
-            </div>
-        </a>
+                </div>
+                <div class="p-2 col-6 ">
+                    <h6 class="Muol-Light f12 line-height-15">
+                        @foreach ($news->newsDetail as $item)
+                            @if (app()->getLocale() == 'kh')
+                                @if ($item->language_id == 1)
+                                    <div onclick="toggleDescription(this)" class="cursor-pointer "
+                                        data-fulltext="{{ $item->header }}">
+                                        {{ Str::limit($item->header, 80) }}
+                                    </div>
+                                @endif
+                            @else
+                                @if ($item->language_id == 2)
+                                    <div onclick="toggleDescription(this)" class="cursor-pointer "
+                                        data-fulltext="{{ $item->header }}">
+                                        {{ Str::limit($item->header, 80) }}
+                                    </div>
+                                @endif
+                            @endif
+                        @endforeach
+                    </h6>
+                    <p class="text-danger">
+                        {{ \DateTime::createFromFormat('Y-m-d', $news->date)->format('d/M/Y') }}
+                    </p>
 
-        <a href="{{ route('school.news.') }}?id=4" class="col-6 d-flex align-items-center border">
-            <div class="max-width-150 overflow-hidden">
-                <img class="w-100" src="{{ asset('asset/img/school/dailyNews/4/8.jpg') }}" alt="">
-            </div>
-            <div class="p-3 ">
-                <h6 class="Muol-Light f12 line-height-15">
-                    សា​លា​ ប៊ែល​ធី​ អន្តរ​ជាតិ​ បាន​រៀប​ចំ ពិធី​ប្រកាស​ទទួល​ស្គាល់​ និង​បញ្ចូល​ក្រុម​ប្រឹក្សា​យុវ​ជន​</h6>
-                <p class="Battambang-Regular f12 line-height-15">​
-                    សា​លា​ ប៊ែល​ធី​ អន្តរ​ជាតិបាន​ប្រារព្ធ​ពិធី​ប្រគល់​សញ្ញា​បត្រ​មធ្យម​សិក្សា​បឋម​ភូមិ​ប៊ែល​ធី​​
-                </p>
-            </div>
-        </a>
+                </div>
+            </a>
+        @endforeach
 
-        <a href="{{ route('school.news.') }}?id=5" class="col-6 d-flex align-items-center border">
-            <div class="max-width-150 overflow-hidden">
-                <img class="w-100" src="{{ asset('asset/img/school/dailyNews/5/1.jpg') }}" alt="">
+        <div class="py-3 d-flex justify-content-end">
+            <div>
+                {{ $charity_news->links() }}
             </div>
-            <div class="p-3 ">
-                <h6 class="Muol-Light f12 line-height-15">
-                    កម្មវិធីប្រគល់ វិញ្ញាបនបត្រជូនដល់ សិក្សានុសិស្សភាសាអង់គ្លេស</h6>
-                <p class="Battambang-Regular f12 line-height-15">​
-                    សាលា​ ប៊ែលធី​ អន្តរ​ជាតិ​ បាន​ប្រារព្ធ​ពិធី​ប្រគល់​វិញ្ញា​បន​បត្រ​ជូន​ដល់​សិស្សា​នុសិស្ស​ភាសា​អង់​គ្លេស​ទូទៅ
-                </p>
-            </div>
-        </a>
-
-
+        </div>
 
     </div>
 
