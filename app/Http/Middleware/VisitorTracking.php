18133f7a20ba20793dec836a5e3979051551388c
sok serey visal <121630407+soksereyvisalBIU@users.noreply.github.com>
2025-03-27 07:56:33 +0700

    Update VisitorTracking.php

--- app/Http/Middleware/VisitorTracking.php
+++ app/Http/Middleware/VisitorTracking.php
@@ -8,7 +8,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Http;
 use App\Models\Country;
 use App\Models\Visitor;
-use Illuminate\Support\Facades\Log;
 
 class VisitorTracking
 {
@@ -23,28 +22,18 @@ class VisitorTracking
         $response = Http::get("https://ipinfo.io/{$ip}/json");
         $data = $response->json();
 
-        Log::info('Visitor data', $data);
-
         $region = $data['region'] ?? 'Unknown';
         $countryName = $data['country'] ?? 'Unknown';
-
-        Log::info('Visitor region', $region);
-        Log::info('Visitor country', $countryName);
 
         $country = Country::firstOrCreate(
             ['name' => $countryName],
             ['region' => $region]
         );
 
-        Log::info('Country', $country);
-
         $visitor = Visitor::firstOrCreate(
             ['ip_address' => $ip],
             ['country_id' => $country->id]
         );
-
-        Log::info('visitor', $visitor);
-        
         $visitor->increment('visits');
         $visitor->save();
 
