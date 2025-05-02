f86cd4ea0d4fab2449d2bf1965b9f5b24a36dba0
sok serey visal <121630407+soksereyvisalBIU@users.noreply.github.com>
2025-05-02 09:51:38 +0700

    Update index.blade.php

--- resources/views/web/client/charity/index.blade.php
+++ resources/views/web/client/charity/index.blade.php
@@ -61,6 +61,10 @@
         <img src="{{ asset('asset/img/construction/youtube.jpg') }}" alt="">
     </div>
 
+    <div class="py-2">
+        <iframe class="w-100" style="aspect-ratio: 16 / 9;" src="https://www.youtube.com/embed/Lp2sQ83uXrU?si=6jTvQPc_Q8wd3-fM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+
 </div>
 
 
