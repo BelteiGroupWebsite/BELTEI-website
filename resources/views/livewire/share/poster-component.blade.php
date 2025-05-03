a0e9dafa57d357a77f5aedc9ecf6ae2c57cce932
sok serey visal <121630407+soksereyvisalBIU@users.noreply.github.com>
2025-05-03 15:10:34 +0700

    Update poster-component.blade.php

--- resources/views/livewire/share/poster-component.blade.php
+++ resources/views/livewire/share/poster-component.blade.php
@@ -1,7 +1,8 @@
 <div class="row gy-2 justify-content-center">
     {{-- Do your work, then step back. --}}
 
-    @foreach ($posters as $poster)
+    @foreach ($posters->sortBy('order_column') as $poster)
+    
         <img data-aos="zoom-out-up" class="w-75 rounded shadow p-0" src="{{ asset('storage/' . $poster->image) }}"
             alt="">
     @endforeach
