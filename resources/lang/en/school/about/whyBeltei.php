97778936291f7c8a6e91c8256bdede78078bd364
sok serey visal <121630407+soksereyvisalBIU@users.noreply.github.com>
2025-08-12 22:59:23 -0700

    update

--- resources/lang/en/school/about/whyBeltei.php
+++ resources/lang/en/school/about/whyBeltei.php
@@ -2,136 +2,69 @@
 
 return [
 
-    'header' => "Why BELTEI",
+    'header' => "Why Study at BELTEI?",
 
 
     "description" => '
     <div>
-    <div>
-        <h5 class="font-bold my-3">1. RECOGNITION</h5>
-        <ul>
-            <li>
-                Officially recognized by the Ministry of Education Youth and Sport according to the Prakas Nº 291 dated on March 25,
-                2003 to all BELTEI campuses.
 
-            </li>
-            <li>
-                BELTEI is recognized by ISO 9001:2008 National Standard from Institute of Standards of Cambodia.
-            </li>
-            <li>
-                BELTEI is recognized by ISO 9001:2008 International Standard from ACM of United Kingdom.
-            </li>
-            <li>
-                -BELTEI Group was awarded as an outstanding organization from 7th TAYO ASEAN Awards.
-            </li>
-            <li>
-                BELTEI students have the same right to take every public examination as those studying at public schools.
-            </li>
-            <li>
-                BELTEI students passed the outstanding student classifications for the city level as well as the country level almost
-                every academic year.
-            </li>
-            <li>
-                BELTEI students, who are transferred to study abroad such as the United States, Australia, Canada, Singapore, and
-                other countries, are accepted to study at the same grade as BELTEI (If they study Grade 10 at BELTEI, they will be
-                allowed to study at the same Grade 10 in those countries).
-            </li>
-        </ul>
+        <p><strong><span>1. RECOGNITIONS</span></strong></p>
+        <p>- Officially recognized by the Ministry of Education Youth and Sport according to the Prakas No 291&nbsp;</span><span>អយក.ប្រក&nbsp;</span><span>dated on March 25,</span><span style="font-size:19px;font-family:DaunPenh;">&nbsp;</span><span>2003.</span></p>
+        <p>- BELTEI is recognized by ISO 9001:2015 National Standard from Institute of Standards of Cambodia and International Standard from ACM and UKAS of United Kingdom.</span></p>
+        <p>- BELTEI Group was awarded as an outstanding organization from 7th TAYO ASEAN Awards.</span></p>
+        <p>- BELTEI Group Honored with the ASEAN+3 Young Entrepreneurs Award.</span></p>
+        <p>- BELTEI students have the same right to take every public examination as those studying at public schools.</span></p>
+        <p>- BELTEI students passed the outstanding student classifications for the city level as well as the country level almost every academic year.</span></p>
+        <p>- BELTEI students, who are transferred to study abroad such as the United States, Australia, Canada, Singapore, and other countries, are accepted to study at the same grade as BELTEI (If they study Grade 10 at BELTEI, they will be allowed to study at the same Grade 10 in those countries).</span></p>
+        <p><strong><span>2. FACILITIES</span></strong></p>
+        <p><span>- BELTEI has its own building so that it can ensure the sustainability of our students&apos; studies.</span></p>
+        <p>- BELTEI has installed with modern teaching equipment in each classroom according to its level and grade such as Laboratory, Library, Robotic, Listening Lab, LCD Projector, Interactive Tablet, TV Program Internet, slogans, sayings, proverbs, formulate posters, and completed modern study facilities.</span></p>
+        <p>- Every classroom has air-conditioners and equipped with CCTV to observe students&apos; study activities and teachers teaching performances.</span></p>
+        <p>- All campus buildings are equipped with 15oKVA generators.&nbsp;</span></p>
+        <p>- All school vans are owned by BELTEI</span><span>.</span></p>
+        <p><strong><span>3. ADVANTAGES</span></strong></p>
+        <p class="text-center mt-2"><strong><u><span>GENERAL EDUCATION</span></u></strong></p>
+        <p>- For the students who transfer from other schools throughout the country, BELTEI assures to fill out the application forms free of charge, and BELTEI students can transfer to study at the public schools without any problem.</span></p>
+        <p>- BELTEI IS E-Learning Program (BELTEI APP).</span></p>
+        <p>- Each Room limited between 30 to 40 students for Elementary School and 30 to 36 students for Secondary School.</span></p>
+        <p>- From Grade 3 to Grade 12, students are required to join study field trip to the Royal Palace, the National Museum of Cambodia, the Tuol Sleng Genocide Museum, the Choeung Ek Genocidal Center, the Win-Win Monument, the Preah Srey I&ccedil;anavarman Museum, Phnom Penh Safari, Phnom Tamao Wildlife Rescue Centre, Phnom Preah Reach Troap, and the former capital of Longvek (Tralaeng Kaeng Pagoda), Kirirom National Park, Sihanoukville , Kompot - Kep City - Phnom Kampong Trach, and the Sambor Prey Kuk Temple, once or twice per year.</span></p>
+        <p>- Provide English program from grade 1 to grade 12.</span></p>
+        <p><span>- TV Programs for grade 1-6.</span></p>
+        <p><span>- Grade 7 and 8 do the Taekwando and Labokathor classes.</span></p>
+        <p>- Provide extra computer courses from grade 6 to grade 8.</span></p>
+        <p>- Sport program for grade 7, 8, &amp; 9.</span></p>
+        <p>- Students from grade 7-12 have opportunity to meet famous guest speakers in the Extracurricular courses.</span></p>
+        <p>- Awareness on drugs and the effects of E-cigarettes (Vape), as well as traffic laws for Grade 7 &amp; Grade 8.</span></p>
+        <p>- Concepts of Buddhism for Grade 9.</span></p>
+        <p><span>- Food safety and Cambodian Youth The digital age for Grade 10.</span></p>
+        <p><span>- The Roles of Youths in Society for Grade 11.</span></p>
+        <p>- Awareness on the National Social Security Fund (NSSF) and the pension scheme, as well as orientation programs for choosing academic majors in higher education (university) for Grade 12.</span></p>
+        <p>- Students from Grade 2 to Grade 12 will participate in Educational Reading, Public Speaking Contest, Writing Contest, Mathematic Contest Program, Debate Program,</span> <span>Let&rsquo;s defense - Youth&rsquo;s perspective for society.</span></p>
+        <p>- Students, who study at BELTEI from grade 10 to grade 12, are eligible to take <strong>Oknha Ly Chheng</strong> Scholarship Exam in July every year, to pursue a Bachelor degree at the overseas universities which have signed the MOUS with BELTEI.</span></p>
+        <p>- Students from grade 7-12 have the real experiments in the laboratory.</span></p>
+        <p>- To be Convenient for all BELTEI students who study full-time for both morning and afternoon, So in the morning Student can study General Education and in the afternoon they can study English class.</span></p>
+        <p>- After graduated from Grade 6, all students will be eligible the BELTEI Primary Certificate.</span></p>
+        <p>- After graduated from Grade 9, all students will be eligible the BELTEI Junior High School Diploma and Provisional Certificate for Lower Secondary School.</span></p>
+        <p>- After graduated from Grade 12, all students will be eligible the BELTEI Senior High School Diploma and this diploma will be also uploaded on BELTEI International School website.</span></p>
+        <p>- BELTEI grade 9 and grade 12 who have passed the outstanding student exam for province, city and Nationwide are awarded.&nbsp;</span></p>
+        <p>-</span><span>&nbsp;</span><span>BELTEI&apos;s Grade 12 graduates get 50 percent scholarship for 4 years Bachelor Degree (BEd) at BELTEI International University.</span></p>
+        <p>- Grade A students get full scholarship (100%) of <strong>Oknha Ly Chheng</strong> to study abroad with allowances.</span></p>
+        <p>- Follow the curriculum of the Ministry of Education Youth and Sport.</span></p>
+        <p class="text-center mt-2"><strong><u><span>ESL PROGRAM</span></u></strong></p>
+        <p>- This program is conducted by following American Standard starting from Preschool 1 - Level 12.</span></p>
+        <p>- Each room is limited between 20 to 25 students.</span></p>
+        <p>- All Text books and Teaching Materials are sponsored and supported by McGraw-HILL Education in the United States.&nbsp;</span></p>
+        <p>- Learn via TV Programs, Songs and Folktale stories.</span></p>
+        <p>- BELTEIIS E-Learning Program (BELTEI IS APP).</span></p>
+        <p>- The students from Level 7 to Level 12 are eligible to take the exam for <strong>Oknha Ly Chheng</strong> Scholarship as the GED students do.</span></p>
+        <p>- From Level 3 to Level 12 students offer the field trip to the National Museum, Resort and other cultural and historical sites once a year.</span></p>
+        <p>- Students from Level 1 up will participate in English Reading Contest, English Speaking Contest, English Writing Contest, and Debate Program.</span></p>
+        <p>- After graduated from Preschool 6, students are awarded Local Certificates which issued by BELTEI Head Quarter.</span></p>
+        <p>- After graduated from Level 6, students are awarded Local Certificates which issued by BELTEI Head Quarter.</span></p>
+        <p>- After graduating from Level 12, students are awarded two kinds of certificates: 1. BELTEI ESL Diploma, which issued by BELTEI Head Quarter. 2. The International certificate TOEFL issued by ETS from USA or IELTS issued by British Council from UK. These certificates will be uploaded on BELTEI International School website.</span></p>
     </div>
-
-    <div>
-        <h5 class="font-bold my-3">2. Facilities</h5>
-        <ul>
-            <li>
-                BELTEI has its own building so that it can ensure the sustainability of our students’ studies.
-            </li>
-            <li>
-                BELTEI has installed modern teaching equipments in each classroom according to its level and grade such as Lab,
-                Library, Listening Lab, LCD Projector, Internet, slogans, sayings, proverbs, formulated posters, and completed modern
-                study facilities.
-            </li>
-            <li>
-                Every classroom has air-conditioners and equipped with cameras to observe students\' study activities and teachers
-                teaching performances.
-            </li>
-            <li>
-                All campus buildings are equipped with 150KVA generators.
-            </li>
-        </ul>
-    </div>
-
-    <div>
-        <h5 class="font-bold my-3">3. Advantages</h5>
-
-        <ul>
-            <h5 class="font-bold my-4 text-center ">Khmer General Education</h5>
-            <li>
-                For the students who transfer from other schools throughout the country, BELTEI assures to fill out the application
-                forms free of charge, and BELTEI students can transfer to study at the public schools without any problem.
-            </li>
-            <li>
-                A 30 to 40-student class for elementary school and a 30 to 36-student class for secondary school.
-            </li>
-            <li>
-                Students from Grade 7 to Grade 12 are traveled for study tour at National Museum and other historical sites once or
-                twice a year.
-            </li>
-            <li>
-                Provide English program from grade 1 to grade 12.
-            </li>
-            <li>
-                Provide extra Computer courses from grade 6 to grade 8.
-            </li>
-            <li>
-                BELTEI\'s Grade 12 graduates get 50 percent scholarship for 4 years Bachelor Degree (BEd) at BELTEI International
-                University.
-            </li>
-            <li>
-                Students, who study at BELTEI from grade 10 to grade 12, are eligible to take Oknha Ly Chheng Scholarship Exam in
-                early July every year, to pursue a Bachelor degree at the overseas universities which have signed the MOUs with BELTEI.
-            </li>
-            <li>
-                BELTEI creates an easy way for students to study Full time class either morning or afternoon class. In the morning,
-                students can study Khmer General Educational class in accordance with the Ministry of Education Youth and Sport\'s study
-                programs and in the afternoon, they can study English with the modern programs followed by the American style.
-            </li>
-        </ul>
-
-        <ul>
-            <h5 class="font-bold my-4 text-center ">ESL Program</h5>
-            <li>
-                This program is conducted by following American Standard starting from Preschool until Level 12.
-            </li>
-            <li>
-                Each room is limited to between 20 to 30 students.
-            </li>
-            <li>
-                All text books, study and teaching materials are sponsored and supported by McGraw-HILL Education in the United
-                States.
-            </li>
-            <li>
-                For Level 4 to 12 students, study tour to the National Museum and other cultural and historical sites are facilitated
-                twice a year and writing report in English.            
-            </li>
-            <li>
-                BELTEI creates and strengthens English atmosphere both in the classroom and in the Institute compound.
-
-            </li>
-            <li>
-                When graduating from Level 6, students are ​awarded Local Certificates which issued by BELTEI Head Office.
-            </li>
-            <li>
-                When graduating from level 12, students are awarded two kinds of certificates:
-
-            </li>
-            <li>
-                1.The Local certificate which issued by BELTEI Head Office.
-                2.The International certificate TOEFL given by ETS from USA or IELTS given by British Council, University of Cambridge
-                from UK and IDP Australia.
-            </li>
-        </ul>
-    </div>
-</div>
+    
+    
     '
     
 ];
