2135e3e92f6cdf0a2ae011f318e60d6e23b7e81a
sok serey visal <121630407+soksereyvisalBIU@users.noreply.github.com>
2025-09-01 12:04:52 +0700

    special quality

--- resources/lang/en/school/mainprogram/khmerdetail/special-quality.php
+++ resources/lang/en/school/mainprogram/khmerdetail/special-quality.php
@@ -2,71 +2,40 @@
 
 return [
     'header' => "Special Qualities",
-    
+
     'description' => '
     <ol>
     <li>
-        BELTEI was officially recognized by the Ministry of Education Youth and Sport according to the Prakas No
-        291
-        អយក.ប្រក dated on March 23, 2003.
+        BELTEI was officially recognized by the Ministry of Education Youth and Sport according to the Prakas No 291 អយក.ប្រក dated on March 23, 2003.
     </li>
     <li>
-        Every student studying at BELTEI International School has the same rights to take every public
-        examination as those
-        at public schools.
+        Every student studying at BELTEI International School has the same rights to take every public examination as those at public schools.
     </li>
     <li>
-        BELTEI’s curriculum is in accordance with the Ministry of Education Youth and Sport’s with extra
-        sessions for
-        English and computer studies. Every Khmer title and sub-title is translated into English. Moreover,
-        Grade nine and
-        twelve students are invited to take a special extra class on Sundays for the subjects of Khmer,
-        Mathematics and
-        Physics free of charge.
+        BELTEI’s curriculum is in accordance with the Ministry of Education Youth and Sport’s with extra sessions for English and computer studies. Every Khmer title and sub-title is translated into English. Moreover, Grade nine and twelve students are invited to take a special extra class from Monday to Saturday for the subjects of Khmer, Mathematics and Physics free of charge.
     </li>
     <li>
-        Almost 100 per cent of BELTEI’s students pass their grades (Grade nine and twelve) and get the place in
-        the
-        Municipal and National contests.
+        Almost 100 percent of BELTEI’s students pass their grades (Grade nine and twelve) and get the place in the Municipal and National contests.
     </li>
     <li>
-        The students who transfer from other schools, BELTEI runs study documents (application form and others)
-        free of
-        charge and BELTEI’s students can transfer to other school without any problem.
+        The students who transfer from other schools, BELTEI runs study documents (application form and others) free of charge and BELTEI’s students can transfer to other school without any problem.
     </li>
     <li>
-        Any student who has finished Grade 11 or failed Grade 12 at public or private schools is not allowed to
-        register for
-        Grade 12 in BELTEI International School. In any case, they would be asked to take the test at the Head
-        Office on
-        Khmer and Mathematics subjects.
+        Any student who has finished Grade 11 or failed Grade 12 at public or private schools is not allowed to register for Grade 12 in BELTEI International School. In any case, they would be asked to take the test at the Head Office on Khmer and Mathematics subjects.
     </li>
     <li>
-        A 30 to 40-student class is equipped with Air Conditioners and Camera.
+        Each Room limited between 30 to 40 students for Elementary School and 30 to 36 students for Secondary School. Class is equipped with Air Conditioners and Camera. BELTEI’s Grade 12 graduates get 50 percent scholarship for 4 and 2-year Bachelor and Associate Degree (BEd & AA) in BELTEI.
+    </li>
+    <li>
+        From Grade 3 to Grade 12, students are required to join study field trip to the Royal Palace, the National Museum of Cambodia, the Tuol Sleng Genocide Museum, the Choeung Ek Genocidal Center, the Win-Win Monument, the Preah Srey Içanavarman Museum, Phnom Penh Safari, Phnom Tamao Wildlife Rescue Centre, Phnom Preah Reach Troap, and the former capital of Longvek (Tralaeng Kaeng Pagoda), Kirirom National Park, Sihanoukville , Kompot - Kep City - Phnom Kampong Trach, and the Sambor Prey Kuk Temple, once or twice per year.
+    </li>
+    <li>
+        BELTEI’s students who intend to transfer their studies to the United States, Australia, Canada, Singapore and other countries are recognized and accepted by the institute in these countries.
+    </li>
+    <li>
+        Students can either study in the morning or afternoon session and students who study General Education in the morning can study English in the afternoon through modern curriculum.
+    </li>
 
-    </li>
-    <li>
-        BELTEI’s Grade 12 graduates get 50 per cent scholarship for 4 and 2-year Bachelor and Associate Degree
-        (BEd & AA) in
-        BELTEI.
-    </li>
-    <li>
-        Students from Grade 4 to Grade 12 are travelled for study tours at National Museum and other historical
-        places once
-        or twice a year.
-    </li>
-    <li>
-        BELTEI’s students who intend to transfer their studies to the United States, Australia, Canada,
-        Singapore and other
-        countries are recognized and accepted by the institute in these countries.
-    </li>
-    <li>
-        Students can either study in the morning or afternoon session and students who study General Education
-        in the
-        morning can study English in the afternoon and vice versa. The books, study and teaching materials are
-        supported by
-        McGRAW-HILL Education, one of the biggest book publishers in the United States.
-    </li>
 </ol>
     '
 ];
