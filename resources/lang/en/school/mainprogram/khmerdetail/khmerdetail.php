8d1ccd6f7c5d694c94948c1903f9a67f60ee49b1
sok serey visal <121630407+soksereyvisalBIU@users.noreply.github.com>
2025-09-01 11:57:00 +0700

    khmer detail

--- resources/lang/en/school/mainprogram/khmerdetail/khmerdetail.php
+++ resources/lang/en/school/mainprogram/khmerdetail/khmerdetail.php
@@ -4,7 +4,7 @@ return [
     'header' => "Vision Mission and Goal",
 
     'description' => '
-    
+
     <div class="content-translate-font">
 
     <h5 class="font-bold my-3">
@@ -12,7 +12,7 @@ return [
     </h5>
 
     <p>
-    Education is a key to open a modern world with full of potentiality of development and a treasure that every human being needs compulsively in achieving comfortable living standards with excellence, peace, freedom and social justice. Apparently, learning is a life long process – learning more and more, learning without any ending, and the knowledge that results from those studies is the most priceless human resource for developing the nation and rapidly reaching the movement of globalization around the world at the present time. The Royal Government of Cambodia led by Samdech Akka Moha Sena Padei Techo Hun Sen, the Prime Minister of Kingdom of Cambodia has set out the Rectangular strategy for Growth employment, Equity, and Efficiency in Cambodia for Capacity Building and Human Resources Development. With strong commitment and high attention on quality of education, BELTEI International School for Khmer General Education has been established, and all learners have been encouraged to practice good activities that are beneficial to themselves, their families, and their nation through providing highly effective education for participating and responding to the Royal Government of Cambodia, which is focusing on strengthening the quality of education, and has regarded the educational field to be a prior field for the development of the country.
+        Education is a key to open a modern world with full of potential of development and a treasure that every human being needs compulsively in achieving comfortable living standards with excellence, peace, freedom and social justice. Apparently, learning is a lifelong process – learning more and more, learning without any ending, and the knowledge that results from those studies is the most priceless human resource for developing the nation and rapidly reaching the movement of globalization around the world at the present time. This is also in accordance with the Royal Government’s Rectangular Strategy led by Samdech Akka Moha Sena Padei Techo HUN SEN, Former Prime minister, Current President of Senate and Chairman of the Supreme Advisory Council of the King, Founder of guaranteed Peace and Development in Cambodia, as well as in accordance with the Pentagonal Strategy of the Royal Government, 7th legislature of the National Assembly led by Samdech Moha Borvor Thipadei HUN MANET, the Prime Minister of Cambodia, an outstanding leader and be fulfilled with ability, who continues to lead the country toward peace and development in all sectors for Growth employment, Equity, and Efficiency in Cambodia for Capacity Building and Human Resources Development. With strong commitment and high attention on quality of education, BELTEI International School for Khmer General Education has been established, and all learners have been encouraged to practice good activities that are beneficial to themselves, their families, and their nation through providing highly effective education for participating and responding to the Royal Government of Cambodia, which is focusing on strengthening the quality of education, and has regarded the educational field to be a prior field for the development of the country.
     </p>
 
     <h5 class="font-bold my-3">
@@ -20,15 +20,7 @@ return [
     </h5>
 
     <p>
-        BELTEI International School will become a private educational institution, which is the leading and most
-        famous
-        institution in Cambodia for educational training on Primary School, Junior-High School, and Senior High
-        School.
-        Students who graduated from BELTEI International School have enough abilities in mental ability, general
-        knowledge,
-        English, Computer skills and they can study further successfully at both local and international
-        universities in
-        accordance with BELTEI’s slogan: <b>"BELTEI, the Best Quality of Education in Cambodia."</b>
+        BELTEL International School will become a leading and well known school in Cambodia with high quality of education in accordance with the national and international standards and it will be worldwide recognized. Students who graduated from BELTEI International School have enough abilities in mental ability, general knowledge, English, Computer, Technical Science and specific skills to challenge both local and international labor markets successfully, in accordance with BELTEI\'s slogan: <b>"BELTEI, the Best Quality of Education in Cambodia"</b>.
     </p>
 
     <h5 class="font-bold my-3">
@@ -37,46 +29,26 @@ return [
 
     <ul>
         <li>
-            Providing education in accordance with the curriculums of the Ministry of Education Youth and Sport’s
-            from Primary
-            to Senior high School and BELTEI adds more than 11,113 words in English to the titles and the sub-titles
-            from Grade
-            1 to grade 12.
+            Providing education in accordance with the curriculums of the Ministry of Education Youth and Sport from Primary to Senior high School and BELTEI with additional hours of English study, translating key words and translating titles for each subject from Grade 1 to Grade 12.
         </li>
         <li>
-            Providing computer studies and extra Sunday classes to Grade 9 and grade12-students.
-
+            Providing computer studies and extra classes from Grade 9 and Grade12 students.
         </li>
         <li>
-            All teachers have experience for many years in teaching; especially, with Teaching Methodology
-            certificates.
-
+            All teachers have experience for many years in teaching; especially, with Teaching Methodology certificates.
         </li>
         <li>
-            Providing study tour programs to the National Museum and other historical sites for applying theory to
-            real living
-            -standards.
+            Providing Field Trips programs to the National Museum and other historical sites for applying theory to real life living-standards.
         </li>
         <li>
-            Providing students with four pillars of education namely, learning for understanding, learning for
-            knowing how to
-            do, learning for being good morality, and learning to live together.
-        </li>
+            Providing students with four pillars of education namely, learning for understanding, learning for knowing how to do, learning for being good morality, and learning to live together.        </li>
         <li>
             Equipping with modern facilities and applying only one standard of BELTEI (BELTEI Standard)
-
         </li>
         <li>
-
-            Strengthening the quality of education, student internal rules, morality, virtue to students frequently
-            avoiding of
-            anti-social activities such as disobeying the state laws, drug abuse, associating with spoiled kids and
-            committing
-            immoral acts in the society.
-        </li>
+            Strengthening the quality of education, student internal rules, morality, virtue to students frequently avoiding of anti-social activities such as disobeying the state laws, drug abuse, associating with spoiled kids and committing immoral acts in the society.        </li>
         <li>
             Providing rewards for out-standing students every semester, every year for city and Country Level.
-
         </li>
     </ul>
 
@@ -86,16 +58,10 @@ return [
     </h5>
 
     <p>
-        By setting the Vision and Mission for boosting the quality of educational field clearly, we strongly believe that
-        students who graduate from grade 12 are passing the National High School Exam 100% prepared by the Ministry of
-        Education, Youth and Sports of Cambodia, and the students are well educated with high standard of education; have
-        enough abilities in mental ability, general knowledge such as Khmer general education, English, Mathematics, Social
-        Studies, Practical Science, life skill, polite behavior, and they will be able to study further successfully at both
-        local and international universities in accordance with BELTEI’s motto: <b>“Quality, Efficiency, Excellence, Morality,
-            Virtue”</b>.
+        By setting the clear Vision and Mission for boosting the quality of educational field, we strongly believe that students who graduate from grade 12 are passing the National High School Exam 100% prepared by the Ministry of Education, Youth and Sports of Cambodia, and the students are well educated with high standard of education; have enough abilities in mental ability, general knowledge such as Khmer general education, English, Mathematics, Social Studies, Practical Science, life skill, polite behavior, and they will be able to study further successfully at both local and international universities in accordance with BELTEI’s motto: <b>"BELTEI, the Best Quality of Education in Cambodia"</b>.
     </p>
 
 </div>
-    
+
     '
 ];
