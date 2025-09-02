dd414a0fe19e83f81e67e69c508a3d792f39326b
sok serey visal <121630407+soksereyvisalBIU@users.noreply.github.com>
2025-09-02 10:27:59 +0700

    chinese

--- resources/views/web/client/school/mainprogram/CSLdetail/schedule.blade.php
+++ resources/views/web/client/school/mainprogram/CSLdetail/schedule.blade.php
@@ -7,9 +7,12 @@
         <h3 class="title-translate-font text-center my-3 underline">ថ្នាក់​សម្រាប់​យុវជន​ ឬ​មនុស្ស​ពេញ​វ័យ​​</h3>
 
         <div>
-            <img class="w-100" src="https://www.beltei.edu.kh/khm/images/study_price/date-time.png" alt="">
+            <img class="w-100" src="{{ asset('asset/img/school/mainprogram/schedule0/1.jpg') }}" alt="">
+            <img class="w-100" src="{{ asset('asset/img/school/mainprogram/schedule0/2.jpg') }}" alt="">
+            <img class="w-100" src="{{ asset('asset/img/school/mainprogram/schedule0/3.jpg') }}" alt="">
+            {{-- <img class="w-100" src="https://www.beltei.edu.kh/khm/images/study_price/date-time.png" alt="">
             <img class="w-100" src="https://www.beltei.edu.kh/khm/images/study_price/subjects-price-1.png" alt="">
-            <img class="w-100" src="https://www.beltei.edu.kh/khm/images/study_price/subjects-price-2.png" alt="">
+            <img class="w-100" src="https://www.beltei.edu.kh/khm/images/study_price/subjects-price-2.png" alt=""> --}}
         </div>
 
     </div>
