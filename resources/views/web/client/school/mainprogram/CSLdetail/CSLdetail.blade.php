588777aca4c96b7c254b74225cd3ad81fc88c8ea
sok serey visal <121630407+soksereyvisalBIU@users.noreply.github.com>
2025-09-02 10:20:29 +0700

    update

--- resources/views/web/client/school/mainprogram/CSLdetail/CSLdetail.blade.php
+++ resources/views/web/client/school/mainprogram/CSLdetail/CSLdetail.blade.php
@@ -11,5 +11,26 @@
             {!! __('school/mainprogram/CSLdetail/CSLdetail.description') !!}
         </div>
 
+        <div class="row">
+
+            <div class="col-4 p-2">
+                <img src="{{ asset('asset/img/school/mainprogram/csl/cert1.jpg') }}" alt="">
+            </div>
+            <div class="col-4 p-2">
+                <img src="{{ asset('asset/img/school/mainprogram/csl/cert2.jpg') }}" alt="">
+            </div>
+            <div class="col-4 p-2">
+                <img src="{{ asset('asset/img/school/mainprogram/csl/cert3.jpg') }}" alt="">
+            </div>
+
+        </div>
+
+        <div class="text-center row">
+            <img class="w-50 p-2" src="{{ asset('asset/img/school/mainprogram/csl/1.jpg') }}" alt="">
+            <img class="w-50 p-2" src="{{ asset('asset/img/school/mainprogram/csl/2.jpg') }}" alt="">
+            <img class="w-50 p-2" src="{{ asset('asset/img/school/mainprogram/csl/3.jpg') }}" alt="">
+            <img class="w-50 p-2" src="{{ asset('asset/img/school/mainprogram/csl/4.jpg') }}" alt="">
+        </div>
+
     </div>
 @endsection
