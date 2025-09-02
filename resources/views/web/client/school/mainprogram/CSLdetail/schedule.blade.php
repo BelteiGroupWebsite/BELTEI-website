534395d173ebebbbf6eba460a4f5b47c844d2d13
sok serey visal <121630407+soksereyvisalBIU@users.noreply.github.com>
2025-09-02 10:40:51 +0700

    update

--- resources/views/web/client/school/mainprogram/CSLdetail/schedule.blade.php
+++ resources/views/web/client/school/mainprogram/CSLdetail/schedule.blade.php
@@ -7,6 +7,7 @@
         <h3 class="title-translate-font text-center my-3 underline">ថ្នាក់​សម្រាប់​យុវជន​ ឬ​មនុស្ស​ពេញ​វ័យ​​</h3>
 
         <div>
+            <img class="w-100" src="{{ asset('asset/img/school/mainprogram/schedule/date-time.png') }}" alt="">
             <img class="w-100" src="{{ asset('asset/img/school/mainprogram/schedule/1.jpg') }}" alt="">
             <img class="w-100" src="{{ asset('asset/img/school/mainprogram/schedule/2.jpg') }}" alt="">
             <img class="w-100" src="{{ asset('asset/img/school/mainprogram/schedule/3.jpg') }}" alt="">
