b342960e8768134b906aacb8c310024868d597ac
sok serey visal <121630407+soksereyvisalBIU@users.noreply.github.com>
2026-06-10 09:35:01 +0700

    Update belteigroup.blade.php

--- resources/views/web/client/beltei_university/about/belteigroup.blade.php
+++ resources/views/web/client/beltei_university/about/belteigroup.blade.php
@@ -6,8 +6,15 @@
     <div class="p-3">
 
         <h2 class="title-translate-font my-3 underline">{{ __('school/about/belteigroup.header') }}</h2>
-        
+
         <div class="row g-3">
+            <div class="col-12">
+                {{-- <img class="w-100" src="{{ asset('assets/images/school/belteigroup.jpg') }}" alt=""> --}}
+                <iframe class="w-100 aspect-ratio-16" src="https://www.youtube.com/embed/hNlIC_dPKBQ?si=fGg8LUbXSvpGLene"
+                    title="YouTube video player" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
             <div class="col-12 col-md-6">
                 <img class="w-100" src="{{ asset('assets/images/school/belteigroup.jpg') }}" alt="">
             </div>
