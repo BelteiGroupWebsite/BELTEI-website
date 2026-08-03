07957400804e7c8c45b5c9af8b7931deb0795e3d
sok serey visal <121630407+soksereyvisalBIU@users.noreply.github.com>
2026-08-03 17:42:32 +0700

    f

--- resources/lang/en/beltei_university/admission/price.php
+++ resources/lang/en/beltei_university/admission/price.php
@@ -6,89 +6,143 @@ return [
 
     'description' => '
 
+<!-- 1. Tuition Fee for National Program -->
 <div>
- <h4 class = "title-translate-font mb-2 text-start"> 1. Tuition Fee for National Program </h4>
- <table class = "table table-hover table-bordered fee-table">
- <tr class = "bg-primary title-translate-font text-white">
- <th> Program </th>
- <th> Year 1 – Year 2 (per Semester) </th>
- <th> Year 3 – Graduation (per Semester) </th>
- </tr>
- <tr>
- <td> Bachelor / Associate Degree </td>
- <td> $ 400 </td>
- <td> $ 450 </td>
- </tr>
- <tr>
- <td> Bachelor / Associate Degree (Faculty of Information Technology) </td>
- <td> $ 450 </td>
- <td> $ 500 </td>
- </tr>
- </table>
- </div>
+  <h4 class="title-translate-font mb-2 text-start"> 1. Tuition Fee for National Program </h4>
+  <table class="table table-hover table-bordered fee-table text-center">
+    <tr class="bg-primary title-translate-font text-white">
+      <th class="text-start">Bachelor / Associate Degree</th>
+      <th>Semester 1</th>
+      <th>Semester 2</th>
+      <th>Semester 3</th>
+      <th>Semester 4</th>
+      <th>Semester 5</th>
+      <th>Semester 6</th>
+      <th>Semester 7</th>
+      <th>Semester 8</th>
+    </tr>
+    <tr>
+      <td class="text-start">General Faculties</td>
+      <td>$400</td>
+      <td>$400</td>
+      <td>$400</td>
+      <td>$400</td>
+      <td>$450</td>
+      <td>$450</td>
+      <td>$450</td>
+      <td>$450</td>
+    </tr>
+    <tr>
+      <td class="text-start">Faculty of Information Technology</td>
+      <td>$450</td>
+      <td>$450</td>
+      <td>$450</td>
+      <td>$450</td>
+      <td>$500</td>
+      <td>$500</td>
+      <td>$500</td>
+      <td>$500</td>
+    </tr>
+  </table>
+</div>
 
- <div>
- <h4 class = "title-translate-font mt-4 mb-2 text-start"> 2. Tuition Fee for International Program </h4>
- <table class = "table table-hover table-bordered">
- <tr class = "bg-warning title-translate-font text-white">
- <th> Program </th>
- <th> Year 1 – Year 2 (per Semester) </th>
- <th> Year 3 – Graduation (per Semester) </th>
- </tr>
- <tr>
- <td> Bachelor / Associate Degree </td>
- <td> $ 500 </td>
- <td> $ 550 </td>
- </tr>
- <tr>
- <td> Bachelor / Associate Degree <span class="text-danger">(Civil Aviation)</span> <img style="max-width: 200px" src="https://beltei.edu.kh/asset/img/university/admission/price/plane.png" alt=""> </td>
- <td> $ 1,000 </td>
- <td> $ 1,200 </td>
- </tr>
- </table>
- </div>
+<!-- 2. Tuition Fee for International Program -->
+<div>
+  <h4 class="title-translate-font mt-4 mb-2 text-start"> 2. Tuition Fee for International Program </h4>
+  <table class="table table-hover table-bordered text-center">
+    <tr class="bg-warning title-translate-font text-white">
+      <th class="text-start">Bachelor / Associate Degree</th>
+      <th>Semester 1</th>
+      <th>Semester 2</th>
+      <th>Semester 3</th>
+      <th>Semester 4</th>
+      <th>Semester 5</th>
+      <th>Semester 6</th>
+      <th>Semester 7</th>
+      <th>Semester 8</th>
+    </tr>
+    <tr>
+      <td class="text-start">General Faculties</td>
+      <td>$500</td>
+      <td>$500</td>
+      <td>$500</td>
+      <td>$500</td>
+      <td>$550</td>
+      <td>$550</td>
+      <td>$550</td>
+      <td>$550</td>
+    </tr>
+    <tr>
+      <td class="text-start">Faculty of Civil Aviation <img style="max-width: 150px" src="https://beltei.edu.kh/asset/img/university/admission/price/plane.png" alt=""></td>
+      <td>$1,000</td>
+      <td>$1,000</td>
+      <td>$1,000</td>
+      <td>$1,000</td>
+      <td>$1,200</td>
+      <td>$1,200</td>
+      <td>$1,200</td>
+      <td>$1,200</td>
+    </tr>
+  </table>
+</div>
 
+<!-- 3. Intensive Course -->
+<div>
+  <h4 class="title-translate-font mt-4 mb-2 text-start"> 3. Intensive Course (English/Chinese Intensive Course and Computer Course) </h4>
+  <table class="table table-hover table-bordered">
+    <tr class="bg-primary title-translate-font text-white">
+      <th>Program</th>
+      <th>Total Fee for General Students</th>
+      <th>Special Fee for Enrolled Students (Year 1, Semester 1)</th>
+    </tr>
+    <tr>
+      <td>1-Year English or Chinese Intensive Course & 1-Year Computer Course</td>
+      <td>$ 700</td>
+      <td>Pay only $ 100</td>
+    </tr>
+  </table>
+</div>
 
+<!-- Day and Time -->
+<div>
+  <h4 class="title-translate-font mt-4 mb-2"> DAY AND TIME </h4>
+  <table class="table table-hover table-bordered">
+    <tr class="bg-primary title-translate-font text-white">
+      <th> Monday – Friday </th>
+      <th> Time </th>
+    </tr>
+    <tr>
+      <td> Morning </td>
+      <td> 08:00AM – 11:15AM </td>
+    </tr>
+    <tr>
+      <td> Afternoon </td>
+      <td> 02:00PM – 05:15PM </td>
+    </tr>
+    <tr>
+      <td> Evening </td>
+      <td> 05:30PM – 08:30PM </td>
+    </tr>
+  </table>
 
- <div>
- <h4 class = "title-translate-font mt-4 mb-2"> DAY AND TIME </h4>
- <table class = "table table-hover table-bordered">
- <tr class = "bg-primary title-translate-font text-white">
- <th> Monday – Friday </th>
- <th> Time </th>
- </tr>
- <tr>
- <td> Morning </td>
- <td> 08:00AM – 11:15AM </td>
- </tr>
- <tr>
- <td> Afternoon </td>
- <td> 02:00PM – 05:15PM </td>
- </tr>
- <tr>
- <td> Evening </td>
- <td> 05:30PM – 08:30PM </td>
- </tr>
- </table>
- <table class = "table table-hover table-bordered">
- <tr class = "bg-primary title-translate-font text-white">
- <th> Saturday – Sunday </th>
- <th> Time </th>
- </tr>
- <tr>
- <td> Saturday </td>
- <td>
- 08:00AM – 11:15AM <br>
- 01:30PM – 05:30PM
- </td>
- </tr>
- <tr>
- <td> Sunday </td>
- <td> 02:00PM – 05:15PM <br> 01:30PM – 05:30PM</td>
- </tr>
- </table>
- </div>
-
+  <table class="table table-hover table-bordered">
+    <tr class="bg-primary title-translate-font text-white">
+      <th> Saturday – Sunday </th>
+      <th> Time </th>
+    </tr>
+    <tr>
+      <td> Saturday </td>
+      <td>
+        08:00AM – 11:15AM <br>
+        01:30PM – 05:30PM
+      </td>
+    </tr>
+    <tr>
+      <td> Sunday </td>
+      <td> 08:00AM – 11:15AM <br> 01:30PM – 05:30PM</td>
+    </tr>
+  </table>
+</div>
 
     '
 
--- resources/lang/kh/beltei_university/admission/price.php
+++ resources/lang/kh/beltei_university/admission/price.php
@@ -5,87 +5,143 @@ return [
     'header' => "តម្លៃសិក្សា",
 
     'description' => '
-
+<!-- ១. តម្លៃសិក្សាកម្មវិធីជាភាសាជាតិ -->
 <div>
-        <h5 class="title-translate-font mb-2 text-start">១. កម្មវិធីសិក្សា ជាភាសាជាតិ (National Program)</h5>
-        <table class="table table-hover table-bordered content-translate-font">
-            <tr class="bg-primary title-translate-font text-white">
-                <td>ថ្នាក់បរិញ្ញាបត្រ/ថ្នាក់បរិញ្ញាបត្ររង</td>
-                <td>ឆ្នាំទី១ ដល់ ឆ្នាំទី២ (ក្នុងមួយឆមាស)</td>
-                <td>ឆ្នាំទី៣ ដល់ ឆ្នាំបញ្ចប់ការសិក្សា (ក្នុងមួយឆមាស)</td>
-            </tr>
-            <tr>
-                <td>ថ្នាក់បរិញ្ញាបត្រ/ថ្នាក់បរិញ្ញាបត្ររង</td>
-                <td>$ 400 / ឆមាស</td>
-                <td>$ 450 / ឆមាស</td>
-            </tr>
-            <tr>
-                <td>ថ្នាក់បរិញ្ញាបត្រ/ថ្នាក់បរិញ្ញាបត្ររង (មហាវិទ្យាល័យព័ត៌មានវិទ្យា)</td>
-                <td>$ 450 / ឆមាស</td>
-                <td>$ 500 / ឆមាស</td>
-            </tr>
-        </table>
-    </div>
+  <h5 class="title-translate-font mb-2 text-start">១. កម្មវិធីសិក្សា ជាភាសាជាតិ (National Program)</h5>
+  <table class="table table-hover table-bordered content-translate-font text-center">
+    <tr class="bg-primary title-translate-font text-white">
+      <th class="text-start">ថ្នាក់បរិញ្ញាបត្រ/បរិញ្ញាបត្ររង</th>
+      <th>ឆមាសទី ១</th>
+      <th>ឆមាសទី ២</th>
+      <th>ឆមាសទី ៣</th>
+      <th>ឆមាសទី ៤</th>
+      <th>ឆមាសទី ៥</th>
+      <th>ឆមាសទី ៦</th>
+      <th>ឆមាសទី ៧</th>
+      <th>ឆមាសទី ៨</th>
+    </tr>
+    <tr>
+      <td class="text-start">មហាវិទ្យាល័យទូទៅ</td>
+      <td>$400</td>
+      <td>$400</td>
+      <td>$400</td>
+      <td>$400</td>
+      <td>$450</td>
+      <td>$450</td>
+      <td>$450</td>
+      <td>$450</td>
+    </tr>
+    <tr>
+      <td class="text-start">មហាវិទ្យាល័យព័ត៌មានវិទ្យា</td>
+      <td>$450</td>
+      <td>$450</td>
+      <td>$450</td>
+      <td>$450</td>
+      <td>$500</td>
+      <td>$500</td>
+      <td>$500</td>
+      <td>$500</td>
+    </tr>
+  </table>
+</div>
 
-    <div>
-        <h5 class="title-translate-font mt-4 mb-2 text-start">២. កម្មវិធីសិក្សា អន្តរជាតិ (International Program)</h5>
-        <table class="table table-hover table-bordered content-translate-font">
-            <tr class="bg-warning title-translate-font text-white">
-                <td>ថ្នាក់បរិញ្ញាបត្រ/ថ្នាក់បរិញ្ញាបត្ររង</td>
-                <td>ឆ្នាំទី១ ដល់ ឆ្នាំទី២ (ក្នុងមួយឆមាស)</td>
-                <td>ឆ្នាំទី៣ ដល់ ឆ្នាំបញ្ចប់ការសិក្សា (ក្នុងមួយឆមាស)</td>
-            </tr>
-            <tr>
-                <td>ថ្នាក់បរិញ្ញាបត្រ/ថ្នាក់បរិញ្ញាបត្ររង</td>
-                <td>$ 500 / ឆមាស</td>
-                <td>$ 550 / ឆមាស</td>
-            </tr>
-            <tr>
-                <td>ថ្នាក់បរិញ្ញាបត្រ/ថ្នាក់បរិញ្ញាបត្ររង <span class="text-danger">(មហាវិទ្យាល័យអាកាសចរស៊ីវិល)</span> <img style="max-width: 200px" src="https://beltei.edu.kh/asset/img/university/admission/price/plane.png" alt=""></td>
-                <td>$ 1,000 / ឆមាស</td>
-                <td>$ 1,200 / ឆមាស</td>
-            </tr>
-        </table>
-    </div>
+<!-- ២. តម្លៃសិក្សាកម្មវិធីអន្តរជាតិ -->
+<div>
+  <h5 class="title-translate-font mt-4 mb-2 text-start">២. កម្មវិធីសិក្សា អន្តរជាតិ (International Program)</h5>
+  <table class="table table-hover table-bordered content-translate-font text-center">
+    <tr class="bg-warning title-translate-font text-white">
+      <th class="text-start">ថ្នាក់បរិញ្ញាបត្រ/បរិញ្ញាបត្ររង</th>
+      <th>ឆមាសទី ១</th>
+      <th>ឆមាសទី ២</th>
+      <th>ឆមាសទី ៣</th>
+      <th>ឆមាសទី ៤</th>
+      <th>ឆមាសទី ៥</th>
+      <th>ឆមាសទី ៦</th>
+      <th>ឆមាសទី ៧</th>
+      <th>ឆមាសទី ៨</th>
+    </tr>
+    <tr>
+      <td class="text-start">មហាវិទ្យាល័យទូទៅ</td>
+      <td>$500</td>
+      <td>$500</td>
+      <td>$500</td>
+      <td>$500</td>
+      <td>$550</td>
+      <td>$550</td>
+      <td>$550</td>
+      <td>$550</td>
+    </tr>
+    <tr>
+      <td class="text-start">មហាវិទ្យាល័យអាកាសចរស៊ីវិល <img style="max-width: 150px" src="https://beltei.edu.kh/asset/img/university/admission/price/plane.png" alt=""></td>
+      <td>$1,000</td>
+      <td>$1,000</td>
+      <td>$1,000</td>
+      <td>$1,000</td>
+      <td>$1,200</td>
+      <td>$1,200</td>
+      <td>$1,200</td>
+      <td>$1,200</td>
+    </tr>
+  </table>
+</div>
 
-    <div>
-        <h5 class="title-translate-font mt-4 mb-2">ថ្ងៃ និង ម៉ោងសិក្សា</h5>
-        <table class="table table-hover table-bordered content-translate-font">
-            <tr class="bg-primary title-translate-font text-white">
-                <td> ច័ន្ទ – សុក្រ </td>
-                <td> ម៉ោង </td>
-            </tr>
-            <tr>
-                <td> ព្រឹក </td>
-                <td> 08:00AM – 11:15AM </td>
-            </tr>
-            <tr>
-                <td> រសៀល </td>
-                <td> 02:00PM – 05:15PM </td>
-            </tr>
-            <tr>
-                <td> ល្ងាច </td>
-                <td> 05:30PM – 08:30PM </td>
-            </tr>
-        </table>
-        <table class="table table-hover table-bordered content-translate-font">
-            <tr class="bg-primary title-translate-font text-white">
-                <td> សៅរ៍ – អាទិត្យ </td>
-                <td> ម៉ោង </td>
-            </tr>
-            <tr>
-                <td> ថ្ងៃសៅរ៍ </td>
-                <td>
-                    08:00AM – 11:15AM <br>
-                    01:30PM – 05:30PM
-                </td>
-            </tr>
-            <tr>
-                <td> ថ្ងៃអាទិត្យ </td>
-                <td> 08:00AM – 11:15AM <br> 01:30PM – 05:30PM</td>
-            </tr>
-        </table>
-    </div>
+<!-- ៣. ថ្នាក់បំប៉ន -->
+<div>
+  <h5 class="title-translate-font mt-4 mb-2 text-start">៣. ថ្នាក់បំប៉ន (English/Chinese Intensive Course and Computer Course)</h5>
+  <table class="table table-hover table-bordered content-translate-font">
+    <tr class="bg-primary title-translate-font text-white">
+      <th>ថ្នាក់បំប៉នភាសាអង់គ្លេស ឬ ភាសាចិន ១ឆ្នាំ និងកុំព្យូទ័រ១ឆ្នាំ</th>
+      <th>តម្លៃសរុបសម្រាប់ សិស្ស-និស្សិតទូទៅ</th>
+      <th>តម្លៃសម្រាប់និស្សិតដែលបាន ចុះឈ្មោះចូលរៀនឆ្នាំទី១ ឆមាសទី១ (កម្មវិធីសិក្សាជាភាសាជាតិ និងអន្តរជាតិ)</th>
+    </tr>
+    <tr>
+      <td>ថ្នាក់បំប៉នភាសាអង់គ្លេស ឬ ភាសាចិន ១ឆ្នាំ និងកុំព្យូទ័រ១ឆ្នាំ</td>
+      <td>$ 700</td>
+      <td>បង់ត្រឹមតែ $ 100 ប៉ុណ្ណោះ</td>
+    </tr>
+  </table>
+</div>
+
+<!-- ថ្ងៃ និង ម៉ោងសិក្សា -->
+<div>
+  <h5 class="title-translate-font mt-4 mb-2">ថ្ងៃ និង ម៉ោងសិក្សា</h5>
+  <table class="table table-hover table-bordered content-translate-font">
+    <tr class="bg-primary title-translate-font text-white">
+      <td> ច័ន្ទ – សុក្រ </td>
+      <td> ម៉ោង </td>
+    </tr>
+    <tr>
+      <td> ព្រឹក </td>
+      <td> 08:00AM – 11:15AM </td>
+    </tr>
+    <tr>
+      <td> រសៀល </td>
+      <td> 02:00PM – 05:15PM </td>
+    </tr>
+    <tr>
+      <td> ល្ងាច </td>
+      <td> 05:30PM – 08:30PM </td>
+    </tr>
+  </table>
+
+  <table class="table table-hover table-bordered content-translate-font">
+    <tr class="bg-primary title-translate-font text-white">
+      <td> សៅរ៍ – អាទិត្យ </td>
+      <td> ម៉ោង </td>
+    </tr>
+    <tr>
+      <td> ថ្ងៃសៅរ៍ </td>
+      <td>
+        08:00AM – 11:15AM <br>
+        01:30PM – 05:30PM
+      </td>
+    </tr>
+    <tr>
+      <td> ថ្ងៃអាទិត្យ </td>
+      <td> 08:00AM – 11:15AM <br> 01:30PM – 05:30PM</td>
+    </tr>
+  </table>
+</div>
     '
 
 ];
