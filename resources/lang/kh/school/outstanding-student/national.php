0ef290ec82fe961ef669919d1ab7c1bb5d5a0747
sok serey visal <121630407+soksereyvisalBIU@users.noreply.github.com>
2026-08-05 09:49:54 +0700

    ុ

--- resources/lang/kh/school/outstanding-student/national.php
+++ resources/lang/kh/school/outstanding-student/national.php
@@ -12,9 +12,9 @@ return
     <div class="content-translate-font table text-start">
     
         <div>
-            <h5 class="title-translate-font bg-success p-2 text-white text-center">ឆ្នាំ២០២២ = ០១នាក់</h5>
+            <h5 class="title-translate-font bg-success p-2 text-white text-center">ឆ្នាំ២០២៥ = ០១នាក់</h5>
             <div class="d-flex align-items-center border">
-                <img src=":image2025" alt="">
+                <img style="max-width: 200px" src=":image2025" alt="">
                 <p>
                     ឆ្នាំសិក្សា២០២៤-២០២៥ សិស្សឈ្មោះ ភឿន នរបុត្រ ថ្នាក់ទី១២ ទទួលបានជ័យលាភីលេខ៤ ក្នុងការប្រឡងជ្រើសរើសសិស្សពូកែទូទាំងប្រទេស រូបវិទ្យា ទទួលបានរង្វាន់លើកទឹកចិត្តចំនួន $៥០០  និងលោកគ្រូ អ្នកគ្រូ ក៏ទទួលបានរង្វាន់លើកទឹកចិត្តចំនួន $៥០០ ផងដែរពី ឯកឧត្តមបណ្ឌិត លី ឆេង តំណាងរាស្រ្តមណ្ឌលរាជធានីភ្នំពេញ ស្ថាបនិក-អគ្គនាយក ប៊ែលធី គ្រុប និងជាប្រធានក្រុមប្រឹក្សាភិបាល នៃសាកលវិទ្យាល័យ ប៊ែលធី អន្តរជាតិ។
                 </p>
@@ -23,7 +23,7 @@ return
         <div>
             <h5 class="title-translate-font bg-success p-2 text-white text-center">ឆ្នាំ២០២២ = ០១នាក់</h5>
             <div class="d-flex align-items-center border">
-                <img src=":image01" alt="">
+                <img style="max-width: 200px" src=":image01" alt="">
                 <p>
                     ឆ្នាំ​សិក្សា​២០២១​-២០២២​ សិស្ស​ឈ្មោះ​​ តែ​ ប៊ុន​ម៉េង​ ថ្នាក់​ទី​១២​ ទទួល​បាន​ជ័យ​លាភី​លេខ​៤​
                     ក្នុង​ការ​ប្រឡង​ជ្រើស​រើស​សិស្ស​ពូកែ​ទូ​ទាំង​ប្រទេស​ ផ្នែក​គណិត​វិទ្យា​
