d4eb15af7cac7dc875e5450b308f10de9b2b049d
sok serey visal <121630407+soksereyvisalBIU@users.noreply.github.com>
2026-08-10 10:38:08 +0700

    Update faculty_detail.blade.php

--- resources/views/web/client/beltei_university/faculty/faculty_detail.blade.php
+++ resources/views/web/client/beltei_university/faculty/faculty_detail.blade.php
@@ -1,117 +1,62 @@
 @extends('web.client.beltei_university.layout.app')
 
 @section('content')
+    @php
+        $id = request()->query('id');
+        $images = [];
 
+        if ($id) {
+            $folderPath = public_path("asset/img/biu/booklets/undergraduate/$id");
 
-    <div class="header">
+            if (File::exists($folderPath)) {
+                // Find all matching image files
+                $files = File::files($folderPath);
 
-    </div>
+                // Sort files naturally (e.g., page_1, page_2, ..., page_10)
+                usort($files, function ($a, $b) {
+                    return strnatcmp($a->getFilename(), $b->getFilename());
+                });
+
+                foreach ($files as $file) {
+                    $extension = strtolower($file->getExtension());
+                    if (in_array($extension, ['jpg', 'jpeg', 'png', 'webp', 'gif'])) {
+                        $images[] = asset("asset/img/biu/booklets/undergraduate/$id/" . $file->getFilename());
+                    }
+                }
+            }
+        }
+    @endphp
+
+    <div class="header"></div>
 
     <div>
-        
         <div class="container my-4 translate-font">
-        
             <div class="bg-header-color p-3 text-white text-center">
-                <h5 id="header-title" class="translate-font">{{ __('beltei_university/faculty/faculty.f' . $_GET['id']) }}</h5>
+                <h5 id="header-title" class="translate-font">{{ __('beltei_university/faculty/faculty.f' . $id) }}</h5>
             </div>
-    
+
             <div class="card">
-                <div class="card-body shadow d-flex justify-content-evenly align-items-center">
-    
-                    {{-- <button id="left" class="btn btn-outline-dark rounded-pill p-3 fa fa-chevron-left"></button> --}}
-                    
-                    <div class="pb-3" id="booklets">
-                        {{-- <img id="booklets" src="{{ asset('asset/img/biu/booklets/undergraduate/BA/it/(1).jpg') }}" alt=""> --}}
-                        {{-- <img class="w-80" id="booklets" src="" alt=""> --}}
+                <div class="card-body shadow d-flex flex-column align-items-center">
+
+                    <div class="pb-3 text-center" id="booklets">
+                        @forelse ($images as $image)
+                            <img class="w-75 mb-3" src="{{ $image }}" alt="Booklet Page">
+                        @empty
+                            <p class="text-muted my-4">No images found for this booklet.</p>
+                        @endforelse
                     </div>
-                    
-                    {{-- <button id="right" name="" class="btn btn-outline-dark rounded-pill p-3 fa fa-chevron-right"></button> --}}
-    
-                    <div class="file-count position-absolute" style="left: .5rem; bottom:.5rem">
-    
-                        {{-- <p class="translate-font">{{ __('beltei_university/faculty/faculty.Page') }} <span id="currentPage"></span> {{ __('beltei_university/faculty/faculty.of') }} <span id="allPages"></span></p> --}}
-                        @php
-                            try {
-                                $id = $_GET['id']; //Retrieve the 'id' parameter from the URL
-                                $folderPath = public_path("asset/img/biu/booklets/undergraduate/$id");
-                                $fileCount = count(glob("$folderPath/*"));
-                                //echo "<p>Page <script>x</script> of  $fileCount</p>";
-                                echo "
-                                <script>
-                                    var id = $id;
-                                    var fileCount = $fileCount;
-                                    document.getElementById('allPages').innerText = fileCount;
-                                </script>
-                                ";
-                            } catch (\Throwable $th) {
-                                //Handle any exceptions here
-                            }
-                        @endphp
-                        
-                    </div>
-    
+
+                    @if (count($images) > 0)
+                        <div class="file-count text-muted">
+                            <p class="translate-font mb-0">
+                                {{ __('beltei_university/faculty/faculty.Page') }} 1
+                                {{ __('beltei_university/faculty/faculty.of') }} {{ count($images) }}
+                            </p>
+                        </div>
+                    @endif
+
                 </div>
             </div>
-    
-    
-    
         </div>
-        
-        
     </div>
-
-
-
-    <script>
-        var x = 1;
-        var header = document.getElementById('header-title');
-        
-        var currentPage = document.getElementById("currentPage");
-        var allPages = document.getElementById("allPages");
-        var booklets = document.getElementById('booklets');
-    
-        booklets.src = `{{ asset('asset/img/biu/booklets/undergraduate/${id}/(${x}).jpg') }}`;
-
-
-        var images = '';
-        for (let i = 1; i <= fileCount; i++) {
-            images += `
-                <img class="w-75" src="{{ asset('asset/img/biu/booklets/undergraduate/${id}/(${i}).jpg') }}" alt=""> 
-            `;
-        }
-        booklets.innerHTML = images;
-        
-        
-        
-        // function updatePage(x){
-        //     currentPage.innerText = x;
-        // }
-        
-        // updatePage(x);
-        
-        // document.getElementById('right').addEventListener('click', function () {
-        //     x++;
-        //     if (x > fileCount) {
-        //         x = 1;
-        //     }
-        //     for(let i = 1 ; i < fileCount ; i++){
-        //         images += `
-        //             <img class="w-80" id="booklets" src="{{ asset('asset/img/biu/booklets/undergraduate/${id}/(${1}).jpg') }}" alt=""> 
-        //         `;
-        //     }
-        //     // booklets.src = `{{ asset('asset/img/biu/booklets/undergraduate/${id}/(${x}).jpg') }}`;
-        //     updatePage(x);
-        // });
-        
-        // document.getElementById('left').addEventListener('click', function () {
-        //     x--;
-        //     if (x < 1) {
-        //         x = fileCount;
-        //     }
-        //     booklets.src = `{{ asset('asset/img/biu/booklets/undergraduate/${id}/(${x}).jpg') }}`;
-        //     updatePage(x);
-        // });
-    
-    </script>
-
 @endsection
